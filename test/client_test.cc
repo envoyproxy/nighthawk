12fe2f0b756be8d67837e5e513f05f56ee084d9a
Otto van der Schaaf <oschaaf@we-amp.com>
2019-04-24 13:54:14 +0200

    Experiment: Mixer integration server for testing
    Putting this out here for discussion:
    This PR absorbs int_server.cc/int_server.h from
    https://github.com/duderino/proxy/blob/jblatt_mixer_fault_testing/test/integration/
    
    Then it goes on to update the end-to-end integration test for the NH client
    to rely on this server for testing, swapping out Envoy's integration
    server. This allows us to ditch the awkward fork() we have to do
    to avoid runtime conflicts and deflake CI.
    
    Also enhanced and added to the existing client tests while playing around with
    the new server, to get a feel for ease of use (which seems to be pretty easy).
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/client_test.cc
+++ test/client_test.cc
@@ -22,75 +22,30 @@
 #include "client/factories_impl.h"
 #include "client/options_impl.h"
 
+#include "test/server/int_server.h"
+
 using namespace std::chrono_literals;
 using namespace testing;
 
 namespace Nighthawk {
 namespace Client {
 
-class ClientTest : public Envoy::BaseIntegrationTest,
-                   public TestWithParam<Envoy::Network::Address::IpVersion> {
+class ClientServerTest : public TestWithParam<Envoy::Network::Address::IpVersion>,
+                         Envoy::Logger::Loggable<Envoy::Logger::Id::testing> {
 public:
-  ClientTest() : Envoy::BaseIntegrationTest(GetParam(), realTime(), readEnvoyConfiguration()) {}
-
-  std::string readEnvoyConfiguration() {
-    Envoy::Filesystem::InstanceImplPosix file_system;
-    std::string envoy_config = file_system.fileReadToEnd(Envoy::TestEnvironment::runfilesPath(
-        "test/test_data/benchmark_http_client_test_envoy.yaml"));
-    return Envoy::TestEnvironment::substitute(envoy_config);
-  }
-
-  void SetUp() override {
-    // We fork the integration test fixture into a child process, to avoid conflicting
-    // runtimeloaders as both NH and the integration server want to own that and we can have only
-    // one. The plan is to move to python for this type of testing, so hopefully we can deprecate
-    // this test and it's peculiar setup with fork/pipe soon.
-    RELEASE_ASSERT(pipe(fd_port_) == 0, "Failed to open pipe");
-    RELEASE_ASSERT(pipe(fd_confirm_) == 0, "Failed to open pipe");
-    pid_ = fork();
-    RELEASE_ASSERT(pid_ >= 0, "Fork failed");
-
-    if (pid_ == 0) {
-      // child process running the integration test server.
-      ares_library_init(ARES_LIB_INIT_ALL);
-      Envoy::Event::Libevent::Global::initialize();
-      initialize();
-      int port = lookupPort("listener_0");
-      int parent_message;
-      RELEASE_ASSERT(write(fd_port_[1], &port, sizeof(port)) == sizeof(port), "write failed");
-      // The parent process writes to fd_confirm_ when it has read the port. This call to read
-      // blocks until that happens.
-      RELEASE_ASSERT(read(fd_confirm_[0], &parent_message, sizeof(parent_message)) ==
-                         sizeof(parent_message),
-                     "Invalid read size");
-      RELEASE_ASSERT(parent_message == port, "Failed to confirm port");
-      // The parent process closes fd_port_ when the test tears down. The read call blocks until it
-      // does that.
-      RELEASE_ASSERT(read(fd_port_[0], &port_, sizeof(port_)) == -1, "read failed");
-      GTEST_SKIP();
-    } else if (pid_ > 0) {
-      RELEASE_ASSERT(read(fd_port_[0], &port_, sizeof(port_)) > 0, "read failed");
-      RELEASE_ASSERT(port_ > 0, "read unexpected port_ value");
-      RELEASE_ASSERT(write(fd_confirm_[1], &port_, sizeof(port_)) == sizeof(port_), "write failed");
-    }
-  }
-
-  void TearDown() override {
-    if (pid_ == 0) {
-      test_server_.reset();
-      fake_upstreams_.clear();
-      ares_library_cleanup();
-    }
-    RELEASE_ASSERT(!close(fd_confirm_[0]), "close failed");
-    RELEASE_ASSERT(!close(fd_confirm_[1]), "close failed");
-    RELEASE_ASSERT(!close(fd_port_[0]), "close failed");
-    RELEASE_ASSERT(!close(fd_port_[1]), "close failed");
-  }
+  ClientServerTest()
+      : transport_socket_factory_(), ip_version_(GetParam()),
+        listening_socket_(
+            Envoy::Network::Utility::parseInternetAddressAndPort(fmt::format(
+                "{}:{}", Envoy::Network::Test::getLoopbackAddressUrlString(ip_version_), 0)),
+            nullptr, true),
+        server_("server", listening_socket_, transport_socket_factory_,
+                Envoy::Http::CodecClient::Type::HTTP1) {}
 
   std::string testUrl() {
-    RELEASE_ASSERT(pid_ > 0, "Unexpected call to testUrl");
     const std::string address = Envoy::Network::Test::getLoopbackAddressUrlString(GetParam());
-    return fmt::format("http://{}:{}/", address, port_);
+    uint16_t port = static_cast<uint16_t>(listening_socket_.localAddress()->ip()->port());
+    return fmt::format("http://{}:{}/", address, port);
   }
 
   const char* getAddressFamilyOptionString() {
@@ -101,24 +56,48 @@ public:
     return ip_version == Envoy::Network::Address::IpVersion::v6 ? "v6" : "v4";
   }
 
-  int port_;
-  pid_t pid_;
-  int fd_port_[2];
-  int fd_confirm_[2];
+protected:
+  Envoy::Network::RawBufferSocketFactory transport_socket_factory_;
+  Envoy::Network::Address::IpVersion ip_version_;
+  Envoy::Network::TcpListenSocket listening_socket_;
+  Mixer::Integration::Server server_;
 };
 
-INSTANTIATE_TEST_SUITE_P(IpVersions, ClientTest,
+INSTANTIATE_TEST_SUITE_P(IpVersions, ClientServerTest,
                          ValuesIn(Envoy::TestEnvironment::getIpVersionsForTest()),
                          Envoy::TestUtility::ipTestParamsToString);
 
-TEST_P(ClientTest, NormalRun) {
+TEST_P(ClientServerTest, NormalRun) {
+  Mixer::Integration::ServerCallbackHelper server_callbacks; // sends a 200 OK to everything
   Main program(Nighthawk::Client::TestUtility::createOptionsImpl(
       fmt::format("foo --address-family {} --duration 2 --rps 10 {}",
                   getAddressFamilyOptionString(), testUrl())));
+  server_.start(server_callbacks);
   EXPECT_TRUE(program.run());
+  EXPECT_EQ(server_callbacks.connectionsAccepted(), 1);
+  EXPECT_GE(server_callbacks.requestsReceived(), 10);
+  // Server does not close its own sockets but instead relies on the client to initate the close
+  EXPECT_EQ(0, server_callbacks.localCloses());
+  // Server sees a client-initiated close for every socket it accepts
+  EXPECT_EQ(server_callbacks.remoteCloses(), server_callbacks.connectionsAccepted());
 }
 
-TEST_P(ClientTest, AutoConcurrencyRun) {
+TEST_P(ClientServerTest, Prefetching) {
+  Mixer::Integration::ServerCallbackHelper server_callbacks; // sends a 200 OK to everything
+  Main program(Nighthawk::Client::TestUtility::createOptionsImpl(fmt::format(
+      "foo --address-family {} --duration 2 --prefetch-connections --connections 25 --rps 10 {}",
+      getAddressFamilyOptionString(), testUrl())));
+  server_.start(server_callbacks);
+  EXPECT_TRUE(program.run());
+  EXPECT_EQ(server_callbacks.connectionsAccepted(), 25);
+  EXPECT_GE(server_callbacks.requestsReceived(), 10);
+  // Server does not close its own sockets but instead relies on the client to initate the close
+  EXPECT_EQ(0, server_callbacks.localCloses());
+  // Server sees a client-initiated close for every socket it accepts
+  EXPECT_EQ(server_callbacks.remoteCloses(), server_callbacks.connectionsAccepted());
+}
+
+TEST_P(ClientServerTest, AutoConcurrencyRun) {
   std::vector<const char*> argv;
   argv.push_back("foo");
   argv.push_back("--concurrency");
@@ -126,19 +105,28 @@ TEST_P(ClientTest, AutoConcurrencyRun) {
   argv.push_back("--address-family");
   argv.push_back(getAddressFamilyOptionString());
   argv.push_back("--duration");
-  argv.push_back("1");
+  argv.push_back("2");
   argv.push_back("--rps");
-  argv.push_back("1");
+  argv.push_back("5");
   argv.push_back("--verbosity");
   argv.push_back("error");
   std::string url = testUrl();
   argv.push_back(url.c_str());
-
+  Mixer::Integration::ServerCallbackHelper server_callbacks; // sends a 200 OK to everything
   Main program(argv.size(), argv.data());
+
+  server_.start(server_callbacks);
   EXPECT_TRUE(program.run());
+  // each worker ought to have created a single connection.
+  EXPECT_EQ(server_callbacks.connectionsAccepted(), PlatformUtils::determineCpuCoresWithAffinity());
+  EXPECT_GE(server_callbacks.requestsReceived(), 1);
+  // Server does not close its own sockets but instead relies on the client to initate the close
+  EXPECT_EQ(0, server_callbacks.localCloses());
+  // Server sees a client-initiated close for every socket it accepts
+  EXPECT_EQ(server_callbacks.remoteCloses(), server_callbacks.connectionsAccepted());
 }
 
-TEST_P(ClientTest, BadRun) {
+TEST_P(ClientServerTest, BadRun) {
   Main program(Nighthawk::Client::TestUtility::createOptionsImpl(
       fmt::format("foo --address-family {} --duration 1 --rps 1 https://unresolveable.host/",
                   getAddressFamilyOptionString())));
