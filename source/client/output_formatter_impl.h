ac8fd62b243e68e46ab1982322b61b83ef6f8bca
Otto van der Schaaf <oschaaf@we-amp.com>
2019-05-06 15:17:05 +0200

    Improve human readable output.
    Fixes https://github.com/envoyproxy/nighthawk/issues/69
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/client/output_formatter_impl.h
+++ source/client/output_formatter_impl.h
@@ -32,6 +32,10 @@ class ConsoleOutputFormatterImpl : public OutputFormatterImpl {
 public:
   ConsoleOutputFormatterImpl(Envoy::TimeSource& time_source, const Options& options);
   std::string toString() const override;
+
+private:
+  std::string statIdtoFriendlyStatName(absl::string_view stat_id) const;
+  std::string formatProtoDuration(const google::protobuf::Duration& duration) const;
 };
 
 class JsonOutputFormatterImpl : public OutputFormatterImpl {
