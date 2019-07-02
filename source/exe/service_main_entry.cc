8e4081e5c72a16a8adddece09c6ee7d76401cbe9
Otto van der Schaaf <oschaaf@we-amp.com>
2019-07-02 18:41:02 +0200

    Save state
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/exe/service_main_entry.cc
+++ source/exe/service_main_entry.cc
@@ -8,6 +8,7 @@
 
 #include "absl/debugging/symbolize.h"
 #include "api/client/service.pb.h"
+#include "tclap/CmdLine.h"
 
 // NOLINT(namespace-nighthawk)
 
@@ -16,18 +17,45 @@ namespace Client {
 
 class ServiceMain {
 public:
-  ServiceMain() {
+  ServiceMain(int argc, char** argv) {
+    const char* descr = "L7 (HTTP/HTTPS/HTTP2) performance characterization tool.";
+    TCLAP::CmdLine cmd(descr, ' ', "PoC"); // NOLINT
+
+    TCLAP::ValueArg<std::string> internet_address_and_port(
+        "", "listen",
+        "The address:port on which the Nighthawk grpc service should listen. Default: "
+        "0.0.0.0:8443.",
+        false, "0.0.0.0:8443", "uint32_t", cmd);
+
+    // TODO(oschaaf): duplicate code, shared with the CLI
+    cmd.setExceptionHandling(false);
+    try {
+      cmd.parse(argc, argv);
+    } catch (TCLAP::ArgException& e) {
+      try {
+        cmd.getOutput()->failure(cmd, e);
+      } catch (const TCLAP::ExitException&) {
+        // failure() has already written an informative message to stderr, so all that's left to do
+        // is throw our own exception with the original message.
+        throw MalformedArgvException(e.what());
+      }
+    } catch (const TCLAP::ExitException& e) {
+      // parse() throws an ExitException with status 0 after printing the output for --help and
+      // --version.
+      throw NoServingException();
+    }
+    // end duplicate code, shared with the CLI. Fix this.
+
+    auto listener_address =
+        Envoy::Network::Utility::parseInternetAddressAndPort(internet_address_and_port.getValue());
     grpc::ServerBuilder builder;
     int grpc_server_port = 0;
-    const std::string loopback_address = "127.0.0.1";
-    //    Envoy::Network::Test::getLoopbackAddressUrlString(GetParam());
-
-    builder.AddListeningPort(fmt::format("{}:0", loopback_address),
-                             grpc::InsecureServerCredentials(), &grpc_server_port);
+    builder.AddListeningPort(listener_address->asString(), grpc::InsecureServerCredentials(),
+                             &grpc_server_port);
     builder.RegisterService(&service_);
     server_ = builder.BuildAndStart();
-    channel_ = grpc::CreateChannel(fmt::format("{}:{}", loopback_address, grpc_server_port),
-                                   grpc::InsecureChannelCredentials());
+    channel_ =
+        grpc::CreateChannel(listener_address->asString(), grpc::InsecureChannelCredentials());
     stub_ = std::make_unique<nighthawk::client::NighthawkService::Stub>(channel_);
   }
 
@@ -36,9 +64,6 @@ public:
   ServiceImpl service_;
   std::unique_ptr<grpc::Server> server_;
   std::shared_ptr<grpc::Channel> channel_;
-  grpc::ClientContext context_;
-  nighthawk::client::ExecutionRequest request_;
-  nighthawk::client::ExecutionResponse response_;
   std::unique_ptr<nighthawk::client::NighthawkService::Stub> stub_;
 };
 
@@ -47,15 +72,14 @@ public:
 
 using namespace Nighthawk::Client;
 
-int main(int, char** argv) {
+int main(int argc, char** argv) {
 #ifndef __APPLE__
   // absl::Symbolize mostly works without this, but this improves corner case
   // handling, such as running in a chroot jail.
   absl::InitializeSymbolizer(argv[0]);
 #endif
   try {
-    ServiceMain service;
-    std::cout << "Server listening" << std::endl;
+    ServiceMain service(argc, argv);
     service.server_->Wait();
     service.Shutdown();
   } catch (const Nighthawk::Client::NoServingException& e) {
