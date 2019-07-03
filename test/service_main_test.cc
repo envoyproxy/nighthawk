b8f74f5d4b7de754638f4207cfcbdd268d183ac1
Otto van der Schaaf <oschaaf@we-amp.com>
2019-07-03 17:13:26 +0200

    Add tests
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/service_main_test.cc
+++ test/service_main_test.cc
@@ -4,6 +4,8 @@
 #include <csignal>
 #include <thread>
 
+#include "nighthawk/common/exception.h"
+
 #include "client/service_main.h"
 
 #include "gtest/gtest.h"
@@ -31,6 +33,24 @@ TEST_F(ServiceMainTest, BadArgs) {
   argv.push_back("bar");
 
   EXPECT_THROW(ServiceMain(argv.size(), argv.data()), std::exception);
+}
+
+TEST_F(ServiceMainTest, BadIpAddress) {
+  std::vector<const char*> argv;
+  argv.push_back("foo");
+  argv.push_back("--listen");
+  argv.push_back("bar");
+
+  EXPECT_THROW(ServiceMain(argv.size(), argv.data()), NighthawkException);
+}
+
+TEST_F(ServiceMainTest, Unbindable) {
+  std::vector<const char*> argv;
+  argv.push_back("foo");
+  argv.push_back("--listen");
+  argv.push_back("255.255.255.255:10");
+  ServiceMain service_main(argv.size(), argv.data());
+  EXPECT_THROW(service_main.Start(), NighthawkException);
 }
 
 } // namespace Client
