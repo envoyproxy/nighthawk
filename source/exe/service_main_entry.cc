3617f76e6d342476799540ac8f179018261e1ba6
Otto van der Schaaf <oschaaf@we-amp.com>
2019-07-03 13:20:20 +0200

    Wrap up --listen argument parsing / handling
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/exe/service_main_entry.cc
+++ source/exe/service_main_entry.cc
@@ -31,21 +31,34 @@ public:
 
     Utility::parseCommand(cmd, argc, argv);
 
-    auto listener_address =
-        Envoy::Network::Utility::parseInternetAddressAndPort(internet_address_and_port.getValue());
+    try {
+      listener_address_ = Envoy::Network::Utility::parseInternetAddressAndPort(
+          internet_address_and_port.getValue());
+    } catch (Envoy::EnvoyException e) {
+      throw NighthawkException(e.what());
+    }
+  }
+
+  void Run() {
     grpc::ServerBuilder builder;
-    int grpc_server_port = 0;
-    builder.AddListeningPort(listener_address->asString(), grpc::InsecureServerCredentials(),
+    int grpc_server_port = listener_address_->ip()->port();
+    builder.AddListeningPort(listener_address_->asString(), grpc::InsecureServerCredentials(),
                              &grpc_server_port);
     builder.RegisterService(&service_);
     server_ = builder.BuildAndStart();
+    if (server_ == nullptr) {
+      throw NighthawkException("Could not start the grpc service.");
+    }
+    std::cout << "Nighthawk grpc service listening: " << listener_address_->asString() << std::endl;
     channel_ =
-        grpc::CreateChannel(listener_address->asString(), grpc::InsecureChannelCredentials());
+        grpc::CreateChannel(listener_address_->asString(), grpc::InsecureChannelCredentials());
     stub_ = std::make_unique<nighthawk::client::NighthawkService::Stub>(channel_);
+    server_->Wait();
   }
 
   void Shutdown() { server_->Shutdown(); }
 
+  Envoy::Network::Address::InstanceConstSharedPtr listener_address_;
   ServiceImpl service_;
   std::unique_ptr<grpc::Server> server_;
   std::shared_ptr<grpc::Channel> channel_;
@@ -65,13 +78,14 @@ int main(int argc, char** argv) {
 #endif
   try {
     ServiceMain service(argc, argv); // NOLINT
-    service.server_->Wait();
+    service.Run();
     service.Shutdown();
   } catch (const Nighthawk::Client::NoServingException& e) {
     return EXIT_SUCCESS;
   } catch (const Nighthawk::Client::MalformedArgvException& e) {
     return EXIT_FAILURE;
   } catch (const Nighthawk::NighthawkException& e) {
+    std::cerr << "Failure: " << e.what() << std::endl;
     return EXIT_FAILURE;
   }
   return 0;
