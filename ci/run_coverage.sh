38ec1e24a2388d6b70db5963c29638d1142125a7
Otto van der Schaaf <oschaaf@we-amp.com>
2019-07-11 18:33:31 +0200

    Bump coverage threshold to 97.3%
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- ci/run_coverage.sh
+++ ci/run_coverage.sh
@@ -32,7 +32,7 @@ if [ "$VALIDATE_COVERAGE" == "true" ]
 then
     COVERAGE_VALUE=$(grep -Po '.*lines[.]*: \K(\d|\.)*' "${COVERAGE_SUMMARY}")
     # TODO(oschaaf): The target is 97.5%, so up this whenever possible in follow ups.
-    COVERAGE_THRESHOLD=96.9
+    COVERAGE_THRESHOLD=97.3
     COVERAGE_FAILED=$(echo "${COVERAGE_VALUE}<${COVERAGE_THRESHOLD}" | bc)
     
     echo "HTML coverage report is in ${COVERAGE_DIR}/coverage.html"
