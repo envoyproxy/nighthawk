ce8edc1b0767a46790cd00a62f791066d42ec502
Otto van der Schaaf <oschaaf@we-amp.com>
2019-08-09 11:38:34 +0200

    back out log level change
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/client/process_impl.cc
+++ source/client/process_impl.cc
@@ -64,7 +64,8 @@ ProcessImpl::ProcessImpl(const Options& options, Envoy::Event::TimeSystem& time_
       access_log_manager_(std::chrono::milliseconds(1000), api_, *dispatcher_, fakelock_,
                           store_root_),
       init_watcher_("Nighthawk", []() {}) {
-  configureComponentLogLevels(spdlog::level::from_str("info"));
+  configureComponentLogLevels(spdlog::level::from_str(
+      nighthawk::client::Verbosity::VerbosityOptions_Name(options_.verbosity())));
 }
 
 const std::vector<ClientWorkerPtr>& ProcessImpl::createWorkers(const UriImpl& uri,
