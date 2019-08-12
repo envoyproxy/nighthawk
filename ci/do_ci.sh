fa039093f5b72b587f642d7d92702a039b163f66
Otto van der Schaaf <oschaaf@we-amp.com>
2019-08-12 11:21:51 +0200

    Whitespace fix
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -6,7 +6,7 @@ export BUILDIFIER_BIN="/usr/local/bin/buildifier"
 
 function do_build () {
     bazel build $BAZEL_BUILD_OPTIONS --verbose_failures=true //:nighthawk_client //:nighthawk_test_server \
-    //:nighthawk_service
+        //:nighthawk_service
 }
 
 function do_test() {
