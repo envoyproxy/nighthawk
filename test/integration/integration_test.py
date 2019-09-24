7141f008cc43bce9a70c74083e8df4e08e0774b1
Otto van der Schaaf <oschaaf@we-amp.com>
2019-09-24 18:46:54 +0200

    Remove x-dist
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/integration/integration_test.py
+++ test/integration/integration_test.py
@@ -10,6 +10,5 @@ import pytest
 if __name__ == '__main__':
   path = os.path.dirname(os.path.realpath(__file__))
   test_selection_arg = sys.argv[1] if len(sys.argv) > 1 else ""
-  r = pytest.main(["--rootdir=" + path, "-k", test_selection_arg, "-x", path, "-n", "20"],
-                  plugins=["xdist"])
+  r = pytest.main(["--rootdir=" + path, "-k", test_selection_arg, "-x", path])
   exit(r)
