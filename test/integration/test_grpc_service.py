c651b84af04c376a15d93dd5d8e8cfe246267b96
Otto van der Schaaf <oschaaf@we-amp.com>
2019-10-03 19:51:19 +0200

    Clean up, add service stress test
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/integration/test_grpc_service.py
+++ test/integration/test_grpc_service.py
@@ -5,7 +5,7 @@ from integration_test_fixtures import (http_test_server_fixture)
 from utility import *
 
 
-def test_grpc_service(http_test_server_fixture):
+def test_grpc_service_happy_flow(http_test_server_fixture):
   http_test_server_fixture.startNighthawkGrpcService()
   parsed_json, _ = http_test_server_fixture.runNighthawkClient([
       "--duration 1", "--rps 10",
@@ -16,3 +16,16 @@ def test_grpc_service(http_test_server_fixture):
   counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
   assertEqual(counters["benchmark.http_4xx"], 10)
   assertEqual(counters["headersource.internal.upstream_rq_200"], 1)
+
+
+def test_grpc_service_stress(http_test_server_fixture):
+  http_test_server_fixture.startNighthawkGrpcService()
+  parsed_json, _ = http_test_server_fixture.runNighthawkClient([
+      "--duration 10", "--rps 20000", "--concurrency 4",
+      "--header-source %s:%s" % (http_test_server_fixture.grpc_service.server_ip,
+                                 http_test_server_fixture.grpc_service.server_port),
+      http_test_server_fixture.getTestServerRootUri()
+  ])
+  counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
+  assertGreaterEqual(counters["benchmark.http_4xx"], 10000)
+  assertEqual(counters["headersource.internal.upstream_rq_200"], 4)
--- test/service_test.cc
+++ test/service_test.cc
@@ -193,22 +193,5 @@ TEST_P(ServiceTest, Unresolvable) {
   runWithFailingValidationExpectations("Unknown failure");
 }
 
-// TODO(oschaaf): this is a hello-world test for coverage.
-// finish it up.
-TEST_P(ServiceTest, HeaderSourceWithValid) {
-  auto options = request_.mutable_start_request()->mutable_options();
-  options->mutable_header_source()->mutable_uri()->set_value(
-      fmt::format("http://{}:{}", loopback_address_, grpc_server_port_));
-  auto r = stub_->ExecutionStream(&context_);
-  r->Write(request_, {});
-  r->WritesDone();
-  EXPECT_TRUE(r->Read(&response_));
-  ASSERT_FALSE(response_.has_error_detail());
-  EXPECT_TRUE(response_.has_output());
-  EXPECT_GE(response_.output().results(0).counters().size(), 8);
-  auto status = r->Finish();
-  EXPECT_TRUE(status.ok());
-}
-
 } // namespace Client
 } // namespace Nighthawk
