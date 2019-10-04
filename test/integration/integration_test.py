8d770adc4f3594ad3a112338832aaa7cd47109aa
Otto van der Schaaf <oschaaf@we-amp.com>
2019-10-04 22:34:15 +0200

    Integration tests: retry server start (#148)
    It's possible that ports(s) we came up with for tests servers to use get stolen in the time it takes
    to get to the test server to boot up.
    
    This moves us to using a combination of generating port 0 bindings in configuration and querying the admin interface for the listeners that we are interested in.
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/integration/integration_test.py
+++ test/integration/integration_test.py
@@ -10,5 +10,19 @@ import pytest
 if __name__ == '__main__':
   path = os.path.dirname(os.path.realpath(__file__))
   test_selection_arg = sys.argv[1] if len(sys.argv) > 1 else ""
-  r = pytest.main(["--rootdir=" + path, "-k", test_selection_arg, "-x", path])
+  r = pytest.main(
+      [
+          "--rootdir=" + path,
+          "-vvvv",
+          "--showlocals",  # Don't abbreviate/truncate long values in asserts.
+          "-p",
+          "no:cacheprovider",  # Avoid a bunch of warnings on readonly filesystems 
+          "-k",
+          test_selection_arg,  # Passed in via BUILD/py_test()
+          "-x",
+          path,
+          "-n",
+          "20"  # Number of tests to run in parallel
+      ],
+      plugins=["xdist"])
   exit(r)
