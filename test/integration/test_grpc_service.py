ac5a8ab65929e41bcdc241944bfd2038e3703cea
Otto van der Schaaf <oschaaf@we-amp.com>
2019-10-04 23:28:07 +0200

    Lower grpc stress test expectations for TSAN runs
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/integration/test_grpc_service.py
+++ test/integration/test_grpc_service.py
@@ -27,5 +27,6 @@ def test_grpc_service_stress(http_test_server_fixture):
       http_test_server_fixture.getTestServerRootUri()
   ])
   counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
-  assertGreaterEqual(counters["benchmark.http_4xx"], 10000)
+  # The low treshold is for TSAN in CI.
+  assertGreaterEqual(counters["benchmark.http_4xx"], 5000)
   assertEqual(counters["headersource.internal.upstream_rq_200"], 4)
