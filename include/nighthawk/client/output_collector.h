8234e682d3e309355f47748ca7deb293805f0dcb
Otto van der Schaaf <oschaaf@we-amp.com>
2019-10-11 06:58:24 +0200

    CLI for transforming output (#159)
    Allows transformations to different output formats.
    This will become more useful as the number of supported output formats grows.
    (Dotted strings and fortio are both expected / in the pipeline).
    
    This first cut support stdin for taking input, expecting json.
    
    Example I/O, transforming json to human output:
    
    bazel-bin/nighthawk_client --duration 1 --output-format json 127.0.0.1 | bazel-bin/nighthawk_output_transform --output-format human
    [11:48:47.109362][16175][I] [source/client/process_impl.cc:171] Starting 1 threads / event loops. Test duration: 1 seconds.
    [11:48:47.109442][16175][I] [source/client/process_impl.cc:173] Global targets: 1 connections and 5 calls per second.
    [11:48:47.121150][16175][I] [external/envoy/source/common/upstream/cluster_manager_impl.cc:162] cm init: all clusters initialized
    [11:48:49.051875][16175][I] [source/client/client.cc:56] Done.
    Nighthawk - A layer 7 protocol benchmarking tool.
    
    Queueing and connection setup latency
      samples: 4
      mean:    0s 000ms 039us
      pstdev:  0s 000ms 025us
    
      Percentile  Count       Latency
      0           1           0s 000ms 019us
      0.5         2           0s 000ms 026us
      0.75        3           0s 000ms 028us
      1           4           0s 000ms 082us
    
    Request start to response end
      samples: 4
      mean:    0s 000ms 334us
      pstdev:  0s 000ms 196us
    
      Percentile  Count       Latency
      0           1           0s 000ms 202us
      0.5         2           0s 000ms 224us
      0.75        3           0s 000ms 236us
      1           4           0s 000ms 673us
    
    Initiation to completion
      samples: 4
      mean:    0s 000ms 423us
      pstdev:  0s 000ms 249us
    
      Percentile  Count       Latency
      0           1           0s 000ms 249us
      0.5         2           0s 000ms 290us
      0.75        3           0s 000ms 299us
      1           4           0s 000ms 854us
    
    Counter                                 Value       Per second
    benchmark.http_2xx                      5           5.00
    cluster_manager.cluster_added           1           1.00
    membership_change                       1           1.00
    runtime.load_success                    1           1.00
    runtime.override_dir_not_exists         1           1.00
    upstream_cx_destroy                     1           1.00
    upstream_cx_destroy_local               1           1.00
    upstream_cx_http1_total                 1           1.00
    upstream_cx_rx_bytes_total              55840       55840.00
    upstream_cx_total                       1           1.00
    upstream_cx_tx_bytes_total              285         285.00
    upstream_rq_pending_total               1           1.00
    upstream_rq_total                       5           5.00
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- include/nighthawk/client/output_collector.h
+++ include/nighthawk/client/output_collector.h
@@ -4,20 +4,20 @@
 
 #include "envoy/common/pure.h"
 
-#include "nighthawk/client/benchmark_client.h"
-#include "nighthawk/client/options.h"
 #include "nighthawk/common/statistic.h"
 
 namespace Nighthawk {
 namespace Client {
 
+/**
+ * Facilitates building up an output proto from Nighthawk's native data structures.
+ */
 class OutputCollector {
 public:
   virtual ~OutputCollector() = default;
   virtual void addResult(absl::string_view name, const std::vector<StatisticPtr>& statistics,
                          const std::map<std::string, uint64_t>& counters) PURE;
   virtual nighthawk::client::Output toProto() const PURE;
-  virtual std::string toString() const PURE;
 };
 
 using OutputCollectorPtr = std::unique_ptr<OutputCollector>;
