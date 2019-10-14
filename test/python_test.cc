a60689b77fc808ac1b89e07a7840b1424ba7e417
Otto van der Schaaf <oschaaf@we-amp.com>
2019-10-15 01:16:49 +0200

    Update Envoy dependency (#164)
    Updates Envoy to 94ac19f5e16ad012aa3a77258c0868c2fea2a7f0
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/python_test.cc
+++ test/python_test.cc
@@ -1,8 +1,4 @@
-#include <vector>
-
-#include "external/envoy/test/mocks/common.h"
-#include "external/envoy/test/test_common/environment.h"
-#include "external/envoy/test/test_common/network_utility.h"
+#include "test/test_common/environment.h"
 
 #include "gtest/gtest.h"
 
@@ -16,8 +12,7 @@ class PythonTest : public Test {};
 // This runs the python integration tests from within a test context, for the purpose
 // of getting code coverage reporting to also consider the code hit by integration tests.
 TEST_F(PythonTest, IntegrationTests) {
-  const std::string path =
-      Envoy::TestEnvironment::runfilesPath("test/integration/integration_test");
+  const std::string path = TestEnvironment::runfilesPath("test/integration/integration_test");
   ASSERT_EQ(0, system(path.c_str()));
 }
 
