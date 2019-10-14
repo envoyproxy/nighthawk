9ca910e5596f3adeb58c7b951adfcc2b98237232
Otto van der Schaaf <oschaaf@we-amp.com>
2019-10-14 10:25:48 +0200

    Add format check exclusion, add TODO
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- tools/check_format.sh
+++ tools/check_format.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+# TODO(https://github.com/envoyproxy/nighthawk/issues/165): fully excluding api/client/BUILD
+# isn't ideal.
 bazel run @envoy//tools:check_format.py -- \
   --skip_envoy_build_rule_check  --namespace_check Nighthawk \
+  --build_fixer_check_excluded_paths=$(realpath "api/client/BUILD") \
   --include_dir_order envoy,nighthawk,external/source/envoy,external,api,common,source,exe,server,client,test_common,test \
   $1 $PWD
 
