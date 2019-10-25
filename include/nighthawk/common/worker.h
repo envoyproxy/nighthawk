4c2f3a2887b7ae63dc37b4b14ef6aeb12945648e
Otto van der Schaaf <oschaaf@we-amp.com>
2019-10-25 10:46:53 +0200

    Postpone and enhance connection pool shutdown (#174)
    Delays termination of the benchmark client, so that the associated
    connection pool shutdown will not influence reported counters.
    Also makes the pool shut down cleanly.
    
    Split out from #167
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- include/nighthawk/common/worker.h
+++ include/nighthawk/common/worker.h
@@ -30,6 +30,12 @@ public:
    * and mandatory.
    */
   virtual void shutdown() PURE;
+
+  /**
+   * Called on-thread after its designated task finishes. Last chance to clean up while the
+   * associated thread is still running.
+   */
+  virtual void shutdownThread() PURE;
 };
 
 using WorkerPtr = std::unique_ptr<Worker>;
