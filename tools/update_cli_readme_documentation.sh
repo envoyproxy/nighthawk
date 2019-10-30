23503637367fba4ca489b5b6bb05fdc3458997f9
Otto van der Schaaf <oschaaf@we-amp.com>
2019-10-30 10:21:56 +0100

    Mind $BAZEL_BUILD_OPTIONS
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- tools/update_cli_readme_documentation.sh
+++ tools/update_cli_readme_documentation.sh
@@ -4,8 +4,8 @@
 
 set -e
 
-bazel run -c dbg //tools:update_cli_readme_documentation -- --binary bazel-bin/nighthawk_client --readme README.md "$@" && \
-bazel run -c dbg //tools:update_cli_readme_documentation -- --binary bazel-bin/nighthawk_service --readme README.md "$@" && \
-bazel run -c dbg //tools:update_cli_readme_documentation -- --binary bazel-bin/nighthawk_output_transform --readme README.md "$@" && \
-bazel run -c dbg //tools:update_cli_readme_documentation -- --binary bazel-bin/nighthawk_test_server --readme source/server/README.md "$@"
+bazel run $BAZEL_BUILD_OPTIONS //tools:update_cli_readme_documentation -- --binary bazel-bin/nighthawk_client --readme README.md "$@"
+bazel run $BAZEL_BUILD_OPTIONS //tools:update_cli_readme_documentation -- --binary bazel-bin/nighthawk_service --readme README.md "$@"
+bazel run $BAZEL_BUILD_OPTIONS //tools:update_cli_readme_documentation -- --binary bazel-bin/nighthawk_output_transform --readme README.md "$@"
+bazel run $BAZEL_BUILD_OPTIONS //tools:update_cli_readme_documentation -- --binary bazel-bin/nighthawk_test_server --readme source/server/README.md "$@"
 
