1e8c497035587990706030c8722203f42ef07678
Otto van der Schaaf <oschaaf@we-amp.com>
2019-11-01 00:12:38 +0100

    Generate the command line docs in README.md (#180)
    - Add tooling to help updating the CLI documentation to the latest, to avoid inconsistencies from creeping in.
    - While testing this I found two enum options (`--verbosity` and `--sequencer-idle-strategy`) displaying their defaults as int values. This PR corrects that.
    - Updates documentation fragments that have CLI `--help` output to the versions generated by this change.
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

---
+++ tools/update_cli_readme_documentation.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# Tool to automatically update README.md files that contain --help output of c++ TCLAP binaries
+
+import logging
+import argparse
+import os
+import pathlib
+import re
+import sys
+import subprocess
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Tool to update README.md CLI documentation.")
+  parser.add_argument(
+      "--binary",
+      required=True,
+      help="Relative path to the target binary, for example: \"bazel-bin/nighthawk_client\".")
+  parser.add_argument(
+      "--readme",
+      required=True,
+      help="Relative path to the target documentation file, for example: \"README.md\"")
+  parser.add_argument(
+      "--mode",
+      default="check",
+      required=True,
+      choices={"check", "fix"},
+      help="Either \"check\" or \"fix\"")
+
+  args = parser.parse_args()
+  logging.getLogger().setLevel(logging.INFO)
+  project_root = os.path.join(os.path.dirname(os.path.join(os.path.realpath(__file__))), "../")
+  # Change directory to avoid TCLAP outputting a full path specification to the binary
+  # in its help output.
+  os.chdir(os.path.realpath(project_root))
+  readme_md_path = os.path.join(project_root, args.readme)
+  process = subprocess.run([args.binary, "--help"], stdout=subprocess.PIPE, check=True)
+
+  # Avoid trailing spaces, as they introduce markdown linting errors.
+  cli_help = [s.strip() for s in process.stdout.decode().splitlines()]
+
+  target_path = pathlib.Path(readme_md_path)
+  with target_path.open("r", encoding="utf-8") as f:
+    original_contents = target_path.read_text(encoding="utf-8")
+    replaced = re.sub("\nUSAGE\:[^.]*.*%s[^```]*" % args.binary, str.join("\n", cli_help),
+                      original_contents)
+
+  if replaced != original_contents:
+    if args.mode == "check":
+      logging.info(
+          "CLI documentation in /%s needs to be updated for %s" % (args.readme, args.binary))
+      sys.exit(-1)
+    elif args.mode == "fix":
+      with target_path.open("w", encoding="utf-8") as f:
+        logging.error(
+            "CLI documentation in /%s needs to be updated for %s" % (args.readme, args.binary))
+        f.write("%s" % replaced)
+
+  logging.info("Done")
+  sys.exit(0)
