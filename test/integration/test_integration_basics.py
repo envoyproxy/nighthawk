df516c56d97702d4d285dac92dc72be71c019e73
Otto van der Schaaf <oschaaf@we-amp.com>
2019-10-31 23:53:24 +0100

    fix to stabilize open-loop mode tests
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/integration/test_integration_basics.py
+++ test/integration/test_integration_basics.py
@@ -120,10 +120,10 @@ def test_http_h1_mini_stress_test_open_loop(http_test_server_fixture):
   """
   counters = mini_stress_test(http_test_server_fixture, [
       http_test_server_fixture.getTestServerRootUri(), "--rps", "2500", "--max-pending-requests",
-      "10", "--duration 10", "--open-loop", "--max-active-requests", "1", "--connections", "1"
+      "1", "--duration 10", "--open-loop", "--max-active-requests", "1", "--connections", "1"
   ])
-  assertCounterGreater(counters, "upstream_rq_pending_total", 10)
-  assertCounterGreater(counters, "upstream_rq_pending_overflow", 10)
+  # we expect pool overflows
+  assertCounterGreater(counters, "benchmark.pool_overflow", 10)
 
 
 def test_http_h2_mini_stress_test_open_loop(http_test_server_fixture):
@@ -132,10 +132,9 @@ def test_http_h2_mini_stress_test_open_loop(http_test_server_fixture):
   """
   counters = mini_stress_test(http_test_server_fixture, [
       http_test_server_fixture.getTestServerRootUri(), "--rps", "2500", "--max-pending-requests",
-      "10", "--duration 10", "--h2", "--open-loop", "--max-active-requests", "1"
+      "1", "--duration 10", "--h2", "--open-loop", "--max-active-requests", "1"
   ])
-  assertCounterEqual(counters, "upstream_rq_pending_total", 1)
-  assertCounterGreater(counters, "upstream_rq_pending_overflow", 10)
+  # we expect pool overflows
   assertCounterGreater(counters, "benchmark.pool_overflow", 10)
 
 
