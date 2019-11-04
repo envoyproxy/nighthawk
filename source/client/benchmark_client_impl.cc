0fcaf9185769ad68e07c51c9e3fee55e0035e6f3
Otto van der Schaaf <oschaaf@we-amp.com>
2019-11-04 22:46:43 +0100

    Review feedback
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/client/benchmark_client_impl.cc
+++ source/client/benchmark_client_impl.cc
@@ -68,10 +68,13 @@ StatisticPtrMap BenchmarkClientHttpImpl::statistics() const {
 };
 
 bool BenchmarkClientHttpImpl::tryStartRequest(CompletionCallback caller_completion_callback) {
-  auto* pool_ptr = pool();
   const uint64_t max_in_flight =
       max_pending_requests_ + (use_h2_ ? max_active_requests_ : connection_limit_);
-  if ((requests_initiated_ - requests_completed_ >= max_in_flight) || pool_ptr == nullptr) {
+  auto* pool_ptr = pool();
+  if (pool_ptr == nullptr) {
+    return false;
+  }
+  if (requests_initiated_ - requests_completed_ >= max_in_flight) {
     // When we allow client-side queueing, we want to have a sense of time spend waiting on that
     // queue. So we return false here to indicate we couldn't initiate a new request.
     return false;
