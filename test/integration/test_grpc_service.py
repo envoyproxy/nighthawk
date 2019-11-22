a4e5fbb0c23c4b0614c53c39fd00bc7fdf634df3
Otto van der Schaaf <oschaaf@we-amp.com>
2019-11-22 21:56:33 +0100

    Fix tests
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/integration/test_grpc_service.py
+++ test/integration/test_grpc_service.py
@@ -8,25 +8,26 @@ from utility import *
 def test_grpc_service_happy_flow(http_test_server_fixture):
   http_test_server_fixture.startNighthawkGrpcService()
   parsed_json, _ = http_test_server_fixture.runNighthawkClient([
-      "--duration 1", "--rps 10",
+      "--termination-predicate", "benchmark.http_2xx:10", "--rps 100",
       "--request-source %s:%s" % (http_test_server_fixture.grpc_service.server_ip,
                                   http_test_server_fixture.grpc_service.server_port),
       http_test_server_fixture.getTestServerRootUri()
   ])
   counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
-  assertEqual(counters["benchmark.http_2xx"], 10)
+  assertGreaterEqual(counters["benchmark.http_2xx"], 10)
   assertEqual(counters["headersource.internal.upstream_rq_200"], 1)
 
 
+@pytest.mark.skipif(isSanitizerRun(), reason="Slow in sanitizer runs")
 def test_grpc_service_stress(http_test_server_fixture):
   http_test_server_fixture.startNighthawkGrpcService()
   parsed_json, _ = http_test_server_fixture.runNighthawkClient([
-      "--duration 10", "--rps 20000", "--concurrency 4",
+      "--duration 100", "--rps 10000", "--concurrency 4", "--termination-predicate",
+      "benchmark.http_2xx:5000",
       "--request-source %s:%s" % (http_test_server_fixture.grpc_service.server_ip,
                                   http_test_server_fixture.grpc_service.server_port),
       http_test_server_fixture.getTestServerRootUri()
   ])
   counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
-  # The low treshold is for TSAN in CI.
   assertGreaterEqual(counters["benchmark.http_2xx"], 5000)
   assertEqual(counters["headersource.internal.upstream_rq_200"], 4)
