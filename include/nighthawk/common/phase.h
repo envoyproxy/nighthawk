3ea80557c687d1a2ee9a2ba40c29de6f9a061ac1
Otto van der Schaaf <oschaaf@we-amp.com>
2019-12-12 13:51:14 +0100

    Add documentation to phase.h
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- include/nighthawk/common/phase.h
+++ include/nighthawk/common/phase.h
@@ -9,14 +9,34 @@
 
 namespace Nighthawk {
 
+/**
+ * Phase represents a distinct phase of a benchmmark execution, like warmup and cooldown.
+ * A phase is associated to a sequencer, which in turn can be associated to separate termination
+ * and failure predicates as well as its own rate limiter policy. The end of a phase also represents
+ * a natural boundary for reporting a snapshot of stats and latencies associated to the phase.
+ * High level, a worker statically configure a vector of phases, and will transfer the hot
+ * connection pool when transitioning between them. At this time, nothing is stopping us from
+ * dynamically injecting phases later, be it via grpc calls and/or live CLI input.
+ */
 class Phase {
 public:
   virtual ~Phase() = default;
 
+  /**
+   * Id of the pase. Should be unique but that is not enforced at this time so take care.
+   * @return absl::string_view
+   */
   virtual absl::string_view id() const PURE;
 
+  /**
+   * @return Sequencer& associated to this phase.
+   */
   virtual Sequencer& sequencer() const PURE;
 
+  /**
+   * Runs the sequencer associated to this phase and blocks until completion, which means this phase
+   * has ended as well.
+   */
   virtual void run() const PURE;
 };
 
