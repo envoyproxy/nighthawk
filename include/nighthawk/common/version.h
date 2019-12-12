dbab3b4808c72e92a9e549b6df1e644731f41b13
Otto van der Schaaf <oschaaf@we-amp.com>
2019-12-12 09:31:43 +0100

    Review feedback + iterate on release procedure
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- include/nighthawk/common/version.h
+++ include/nighthawk/common/version.h
@@ -6,10 +6,13 @@ namespace Nighthawk {
 
 const int MAJOR_VERSION{0};
 const int MINOR_VERSION{3};
+const int PATCH_VERSION{0};
 
 class VersionUtils {
 public:
-  static std::string VersionString() { return absl::StrCat(MAJOR_VERSION, ".", MINOR_VERSION); }
+  static std::string VersionString() {
+    return absl::StrCat(MAJOR_VERSION, ".", MINOR_VERSION, ".", PATCH_VERSION);
+  }
 };
 
 } // namespace Nighthawk
