334fa1281e43f78b5b4a1cec6052d7ab98a5423e
Otto van der Schaaf <oschaaf@we-amp.com>
2019-12-19 08:40:23 +0100

    Review: quoting, whitespace safety for expansions
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- ci/docker/docker_build.sh
+++ ci/docker/docker_build.sh
@@ -13,26 +13,26 @@ set -e
 DOCKER_NAME="nighthawk"
 DOCKER_IMAGE_PREFIX="envoyproxy/${DOCKER_NAME}"
 BINARIES=(nighthawk_test_server nighthawk_client nighthawk_service nighthawk_output_transform)
-BAZEL_BIN=$(bazel info -c opt bazel-bin)
-WORKSPACE=$(bazel info workspace)
-TMP_DIR=${WORKSPACE}/tmp-docker-build-context
+BAZEL_BIN="$(bazel info -c opt bazel-bin)"
+WORKSPACE="$(bazel info workspace)"
+TMP_DIR="${WORKSPACE}/tmp-docker-build-context"
 
-rm -rf ${TMP_DIR}
+rm -rf "${TMP_DIR}"
 
 echo "Preparing docker build context in ${TMP_DIR}"
-cp -r ${WORKSPACE}/ci/docker/ ${TMP_DIR}/
+cp -r "${WORKSPACE}/ci/docker/" "${TMP_DIR}/"
 
 for BINARY in "${BINARIES[@]}"; do
     echo "Copy and strip ${BINARY}"
-    TARGET=${TMP_DIR}/${BINARY}
+    TARGET="${TMP_DIR}/${BINARY}"
     # Docker won't follow symlinks
-    cp ${BAZEL_BIN}/${BINARY} ${TARGET}
-    chmod +w ${TARGET}
-    strip --strip-debug ${TARGET}
+    cp "${BAZEL_BIN}/${BINARY}" "${TARGET}"
+    chmod +w "${TARGET}"
+    strip --strip-debug "${TARGET}"
 done
 
-cd ${TMP_DIR}
+cd "${TMP_DIR}"
 echo "running docker build ... "
-docker build -f ${TMP_DIR}/Dockerfile-${DOCKER_NAME} -t "${DOCKER_IMAGE_PREFIX}-dev:latest" .
-rm -rf ${TMP_DIR}
+docker build -f "${TMP_DIR}/Dockerfile-${DOCKER_NAME}" -t "${DOCKER_IMAGE_PREFIX}-dev:latest" .
+rm -rf "${TMP_DIR}"
 echo "docker build finished"
