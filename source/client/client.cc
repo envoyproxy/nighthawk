36093867ee0ea60458d194101727a476b77a5dde
Otto van der Schaaf <oschaaf@we-amp.com>
2019-12-22 11:53:51 +0100

    PoC: nighthawk_client remote execution option
    Transparently performs a single-shot remote execution of gRPC
    via a running nighthawk_service instance.
    
    This seems low hanging fruit, and potentially a pretty useful
    capabillity to add.
    
    Currently hard-coded as an experiment, but obviously an option
    to enable/configure this would have te be added, e.g.
    ```
    --remote-nighthawk-service 127.0.0.1:8443
    ```
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/client/client.cc
+++ source/client/client.cc
@@ -1,5 +1,7 @@
 #include "client/client.h"
 
+#include <grpc++/grpc++.h>
+
 #include <chrono>
 #include <fstream>
 #include <iostream>
@@ -18,6 +20,7 @@
 #include "external/envoy/source/common/thread_local/thread_local_impl.h"
 
 #include "api/client/output.pb.h"
+#include "api/client/service.grpc.pb.h"
 
 #include "common/frequency.h"
 #include "common/uri_impl.h"
@@ -40,20 +43,53 @@ Main::Main(int argc, const char* const* argv)
 Main::Main(Client::OptionsPtr&& options) : options_(std::move(options)) {}
 
 bool Main::run() {
+  bool res = false;
+  OutputFormatterFactoryImpl output_formatter_factory;
+  auto formatter = output_formatter_factory.create(options_->outputFormat());
   Envoy::Thread::MutexBasicLockable log_lock;
 
+  // TODO(oschaaf): separate bugfix PR for this, we need to lowercase.
+  std::string lower = absl::AsciiStrToLower(
+      nighthawk::client::Verbosity::VerbosityOptions_Name(options_->verbosity()));
   auto logging_context = std::make_unique<Envoy::Logger::Context>(
-      spdlog::level::from_str(
-          nighthawk::client::Verbosity::VerbosityOptions_Name(options_->verbosity())),
-      "[%T.%f][%t][%L] %v", log_lock, false);
-  Envoy::Event::RealTimeSystem time_system; // NO_CHECK_FORMAT(real_time)
-  ProcessImpl process(*options_, time_system);
-  OutputFormatterFactoryImpl output_formatter_factory;
-  OutputCollectorImpl output_collector(time_system, *options_);
-  const bool res = process.run(output_collector);
-  auto formatter = output_formatter_factory.create(options_->outputFormat());
-  std::cout << formatter->formatProto(output_collector.toProto());
-  process.shutdown();
+      spdlog::level::from_str(lower), "[%T.%f][%t][%L] %v", log_lock, false);
+  // Single shot remote execution PoC.
+  // TODO(oschaaf): optionize, e.g: --remote-nighthawk-service 127.0.0.1:8443
+  if (true) {
+    auto channel = grpc::CreateChannel(fmt::format("{}:{}", "127.0.0.1", 8443),
+                                       grpc::InsecureChannelCredentials());
+    auto stub = std::make_unique<nighthawk::client::NighthawkService::Stub>(channel);
+    grpc::ClientContext context;
+    nighthawk::client::ExecutionRequest request;
+    nighthawk::client::ExecutionResponse response;
+
+    *request.mutable_start_request()->mutable_options() = *options_->toCommandLineOptions();
+    auto r = stub->ExecutionStream(&context);
+
+    if (r->Write(request, {}) && r->Read(&response)) {
+      if (response.has_output()) {
+        std::cout << formatter->formatProto(response.output());
+      } else {
+        ENVOY_LOG(error, "remote execution failed");
+      }
+      if (response.has_error_detail()) {
+        ENVOY_LOG(error, "have error detail: {}", response.error_detail().DebugString());
+      }
+      if (!r->WritesDone()) {
+        ENVOY_LOG(warn, "writeDone() failed");
+      } else {
+        auto status = r->Finish();
+        res = status.ok();
+      }
+    }
+  } else {
+    Envoy::Event::RealTimeSystem time_system; // NO_CHECK_FORMAT(real_time)
+    ProcessImpl process(*options_, time_system);
+    OutputCollectorImpl output_collector(time_system, *options_);
+    res = process.run(output_collector);
+    std::cout << formatter->formatProto(output_collector.toProto());
+    process.shutdown();
+  }
   if (!res) {
     ENVOY_LOG(error, "An error ocurred.");
   } else {
