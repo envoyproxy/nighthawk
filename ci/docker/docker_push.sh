66a18416c6b2344c7201d865429e889ab35c34e2
Otto van der Schaaf <oschaaf@we-amp.com>
2020-01-02 16:33:59 +0100

    Update login line
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- ci/docker/docker_push.sh
+++ ci/docker/docker_push.sh
@@ -1,7 +1,7 @@
 set -e
 set +x
 
-docker login
+docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
 SOURCE_IMAGE="envoyproxy/nighthawk-dev:latest"
 PUSH_IMAGE="envoyproxy/nighthawk-dev:latest"
 docker tag "${SOURCE_IMAGE}" "${PUSH_IMAGE}"
