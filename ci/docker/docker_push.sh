f07c31454c2e4a19c689c89c9fe56f23e16ef0a0
Otto van der Schaaf <oschaaf@we-amp.com>
2020-01-02 17:29:35 +0100

    Review feedback: sync to envoy's push script
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- ci/docker/docker_push.sh
+++ ci/docker/docker_push.sh
@@ -1,8 +1,23 @@
+#!/bin/bash
+
+# Do not ever set -x here, it is a security hazard as it will place the credentials below in the
+# CircleCI logs.
 set -e
 set +x
 
-docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
-SOURCE_IMAGE="envoyproxy/nighthawk-dev:latest"
-PUSH_IMAGE="envoyproxy/nighthawk-dev:latest"
-docker tag "${SOURCE_IMAGE}" "${PUSH_IMAGE}"
-docker push "${PUSH_IMAGE}"
+if [[ -n "$CIRCLE_PULL_REQUEST" ]]; then
+    echo 'Ignoring PR branch for docker push.'
+    exit 0
+fi
+
+DOCKER_IMAGE_PREFIX="${DOCKER_IMAGE_PREFIX:-envoyproxy/nighthawk}"
+
+# push the nighthawk image on tags or merge to master
+if [[ -n "$CIRCLE_TAG" ]] || [[  "$CIRCLE_BRANCH" = 'master' ]]; then
+    docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
+    docker push "${DOCKER_IMAGE_PREFIX}-dev:latest"
+    docker tag "${DOCKER_IMAGE_PREFIX}-dev:latest" "${DOCKER_IMAGE_PREFIX}-dev:${CIRCLE_SHA1}"
+    docker push "${DOCKER_IMAGE_PREFIX}-dev:${CIRCLE_SHA1}"
+else
+    echo 'Ignoring non-master branch for docker push.'
+fi
