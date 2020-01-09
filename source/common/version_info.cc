f7b8eaf677b922f54b2fe0722048aecbaf4bf111
Otto van der Schaaf <oschaaf@we-amp.com>
2020-01-09 13:49:30 +0100

    Fix includes & small tweaks
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/common/version_info.cc
+++ source/common/version_info.cc
@@ -1,20 +1,18 @@
 #include "common/version_info.h"
 
-#include <map>
-#include <regex>
 #include <string>
 
-#include "common/common/fmt.h"
-#include "common/common/macros.h"
+#include "external/envoy/source/common/common/macros.h"
 
 #include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/str_split.h"
 #include "absl/strings/string_view.h"
 
 namespace Nighthawk {
 
 const std::string& VersionInfo::version() {
-  CONSTRUCT_ON_FIRST_USE(std::string, fmt::format("{}", NIGHTHAWK_BUILD_VERSION_NUMBER));
+  CONSTRUCT_ON_FIRST_USE(std::string, NIGHTHAWK_BUILD_VERSION_NUMBER);
 }
 
 const envoy::config::core::v3alpha::BuildVersion& VersionInfo::buildVersion() {
@@ -42,7 +40,7 @@ envoy::config::core::v3alpha::BuildVersion VersionInfo::makeBuildVersion(const c
 const std::string
 VersionInfo::toVersionString(const envoy::config::core::v3alpha::BuildVersion& build_version) {
   const auto& version = build_version.version();
-  return fmt::format("{}.{}.{}", version.major(), version.minor(), version.patch());
+  return absl::StrCat(version.major(), ".", version.minor(), ".", version.patch());
 }
 
 } // namespace Nighthawk
