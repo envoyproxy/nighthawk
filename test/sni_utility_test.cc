d915684fbbfd81630369b6fe99444df737a4ddc6
Otto van der Schaaf <oschaaf@we-amp.com>
2020-01-17 21:06:37 +0100

    Review feedback
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/sni_utility_test.cc
+++ test/sni_utility_test.cc
@@ -13,17 +13,15 @@ namespace Client {
 
 class SniUtilityTest : public Test {
 public:
-  void checkSniHostComputation(const std::vector<std::string>& uris,
-                               const std::vector<std::string>& request_headers,
-                               const Envoy::Http::Protocol protocol,
-                               absl::string_view expected_sni_host) {
+  std::string checkSniHostComputation(const std::vector<std::string>& uris,
+                                      const std::vector<std::string>& request_headers,
+                                      const Envoy::Http::Protocol protocol) {
     std::vector<UriPtr> parsed_uris;
     parsed_uris.reserve(uris.size());
     for (const std::string& uri : uris) {
       parsed_uris.push_back(std::make_unique<UriImpl>(uri));
     }
-    std::string sni_host = SniUtility::computeSniHost(parsed_uris, request_headers, protocol);
-    EXPECT_EQ(sni_host, expected_sni_host);
+    return SniUtility::computeSniHost(parsed_uris, request_headers, protocol);
   }
 };
 
@@ -32,14 +30,16 @@ TEST_F(SniUtilityTest, SniHostComputation) {
       Envoy::Http::Protocol::Http10, Envoy::Http::Protocol::Http11, Envoy::Http::Protocol::Http2,
       Envoy::Http::Protocol::Http3};
   for (const auto protocol : all_protocols) {
-    checkSniHostComputation({"localhost"}, {}, protocol, "localhost");
-    checkSniHostComputation({"localhost:81"}, {}, protocol, "localhost");
-    checkSniHostComputation({"localhost"}, {"Host: foo"}, protocol, "foo");
-    checkSniHostComputation({"localhost:81"}, {"Host: foo"}, protocol, "foo");
+    EXPECT_EQ(checkSniHostComputation({"localhost"}, {}, protocol), "localhost");
+    EXPECT_EQ(checkSniHostComputation({"localhost:81"}, {}, protocol), "localhost");
+    EXPECT_EQ(checkSniHostComputation({"localhost"}, {"Host: foo"}, protocol), "foo");
+    EXPECT_EQ(checkSniHostComputation({"localhost:81"}, {"Host: foo"}, protocol), "foo");
     const std::string expected_sni_host(protocol >= Envoy::Http::Protocol::Http2 ? "foo"
                                                                                  : "localhost");
-    checkSniHostComputation({"localhost"}, {":authority: foo"}, protocol, expected_sni_host);
-    checkSniHostComputation({"localhost:81"}, {":authority: foo"}, protocol, expected_sni_host);
+    EXPECT_EQ(checkSniHostComputation({"localhost"}, {":authority: foo"}, protocol),
+              expected_sni_host);
+    EXPECT_EQ(checkSniHostComputation({"localhost:81"}, {":authority: foo"}, protocol),
+              expected_sni_host);
   }
 }
 
