9d8baf57babe998ce19a3dc386de945674c76448
Peter Morgan <pemor@github.com>
2020-01-27 17:54:05 -0500

    Fix build on MAC devices by linking w/ pthread_yield_np on MAC
    Signed-off-by: Peter Morgan <pemor@github.com>

--- source/common/platform_util_impl.h
+++ source/common/platform_util_impl.h
@@ -12,7 +12,13 @@ using namespace std::chrono_literals;
 
 class PlatformUtilImpl : public PlatformUtil {
 public:
-  void yieldCurrentThread() const override { pthread_yield(); }
+  void yieldCurrentThread() const override {
+#ifdef __APPLE__
+      pthread_yield_np();
+#else
+      pthread_yield();
+#endif
+    }
   void sleep(std::chrono::microseconds duration) const override {
     std::this_thread::sleep_for(duration); // NO_CHECK_FORMAT(real_time)
   };
