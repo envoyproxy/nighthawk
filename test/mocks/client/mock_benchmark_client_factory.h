12b365cac2f9739843e404a637c86a6cb14b1aba
Otto van der Schaaf <oschaaf@we-amp.com>
2020-02-22 10:36:44 +0100

    Whoops, add missing files
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

---
+++ test/mocks/client/mock_benchmark_client_factory.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "envoy/api/api.h"
+#include "envoy/event/dispatcher.h"
+#include "envoy/upstream/cluster_manager.h"
+
+#include "nighthawk/client/factories.h"
+
+#include "gmock/gmock.h"
+
+namespace Nighthawk {
+namespace Client {
+
+class MockBenchmarkClientFactory : public BenchmarkClientFactory {
+public:
+  MockBenchmarkClientFactory();
+  MOCK_CONST_METHOD7(create,
+                     BenchmarkClientPtr(Envoy::Api::Api&, Envoy::Event::Dispatcher&,
+                                        Envoy::Stats::Scope&, Envoy::Upstream::ClusterManagerPtr&,
+                                        Envoy::Tracing::HttpTracerPtr&, absl::string_view,
+                                        RequestSource& request_generator));
+};
+
+} // namespace Client
+} // namespace Nighthawk
