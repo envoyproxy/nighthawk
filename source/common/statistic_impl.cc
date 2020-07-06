77249b978ec1ed5c546fe034f2209f667c8966ed
qqustc <qqustc@gmail.com>
2020-07-06 17:09:18 -0400

    Update statistic_impl.cc
    Signed-off-by: Qin Qin <qqin@google.com>

--- source/common/statistic_impl.cc
+++ source/common/statistic_impl.cc
@@ -253,7 +253,7 @@ nighthawk::client::Statistic CircllhistStatistic::toProto(SerializationDomain do
   if (count() == 0)
     return proto;
 
-  std::vector<double> quantiles{0, 0.25, 0.5, 0.75, 0.90, 0.95, 0.99, 0.995, 0.999, 1};
+  std::vector<double> quantiles{0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.775, 0.8, 0.825, 0.85, 0.875, 0.90, 0.925, 0.95, 0.975, 0.99, 0.995, 0.999, 1};
   std::vector<double> computed_quantiles(quantiles.size(), 0.0);
   hist_approx_quantile(histogram_, quantiles.data(), quantiles.size(), computed_quantiles.data());
   for (size_t i = 0; i < quantiles.size(); i++) {
