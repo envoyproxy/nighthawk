06a6a027adeb4970c853ad2f646acf28027e3fb6
Otto van der Schaaf <oschaaf@we-amp.com>
2020-07-07 19:34:39 +0200

    CI: Test that we can produce artifacts with gcc (#397)
    - First iteration on testing gcc support in CI. This restricts the
    task to just building Nighthawk, and not testing it yet, as that
    needs a change in Envoy to go in first.
    - Small cleanup, and a bugfix for those who use the script to run
    locally, which would be detected as running in CircleCI.
    
    Related issue: https://github.com/envoyproxy/nighthawk/issues/382
    (needs a follow up to close, left in as a code-level todo).
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- tools/valgrind.sh
+++
@@ -1,1 +0,0 @@
-valgrind --leak-check=full --track-origins=yes --gen-suppressions=all --suppressions=nighthawk/envoy/tools/valgrind-suppressions.txt --suppressions=nighthawk/tools/valgrind-suppressions.txt bazel-bin/nighthawk_client --rps 2 http://127.0.0.1:10000/
