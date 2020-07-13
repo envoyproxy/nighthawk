f73bc66aa9084099a1f94cb263ec7a7f14b622b2
eric846 <56563761+eric846@users.noreply.github.com>
2020-07-13 17:27:45 -0400

    add proto validation rules, fix comments
    Signed-off-by: eric846 <56563761+eric846@users.noreply.github.com>

--- include/nighthawk/adaptive_load/adaptive_load_controller.h
+++ include/nighthawk/adaptive_load/adaptive_load_controller.h
@@ -14,9 +14,9 @@ namespace AdaptiveLoad {
 // MetricSpecs, and adjusts load up or down based on the result; returns an error if convergence is
 // not detected before the deadline in the spec. Load adjustments and convergence detection are
 // computed by a StepController plugin. Metric values are obtained through MetricsPlugins. Testing
-// Stage: When the optimal load is found, runs one long benchmark to validate it. If
-// |diagnostic_ostream| is non-null, progress messages will be written to it. |time_source| should
-// be nullptr except in tests.
+// Stage: When the optimal load is found, runs one long benchmark to validate it. Progress messages
+// are written to |diagnostic_ostream| such as std::cerr or a std::stringstream. |time_source| can
+// be an Envoy::Event::RealTimeSystem constructed from scratch. NO_CHECK_FORMAT(real_time)
 nighthawk::adaptive_load::AdaptiveLoadSessionOutput
 PerformAdaptiveLoadSession(nighthawk::client::NighthawkService::Stub* nighthawk_service_stub,
                            const nighthawk::adaptive_load::AdaptiveLoadSessionSpec& spec,
