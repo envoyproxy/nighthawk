be4533c32956432ba7e50ac71c10a057b678106d
Qin Qin <qqin@google.com>
2020-07-15 17:58:36 -0400

    fix mock_benchmark_client_factory.h
    Signed-off-by: Qin Qin <qqin@google.com>

--- test/mocks/client/mock_benchmark_client_factory.h
+++ test/mocks/client/mock_benchmark_client_factory.h
@@ -14,7 +14,7 @@ namespace Client {
 class MockBenchmarkClientFactory : public BenchmarkClientFactory {
 public:
   MockBenchmarkClientFactory();
-  MOCK_CONST_METHOD7(create,
+  MOCK_CONST_METHOD8(create,
                      BenchmarkClientPtr(Envoy::Api::Api&, Envoy::Event::Dispatcher&,
                                         Envoy::Stats::Scope&, Envoy::Upstream::ClusterManagerPtr&,
                                         Envoy::Tracing::HttpTracerSharedPtr&, absl::string_view,
