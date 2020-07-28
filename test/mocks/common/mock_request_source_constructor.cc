38c50845b802655e41d552152fa1fb9c4ae3aa5e
William Juan <66322422+wjuan-AFK@users.noreply.github.com>
2020-07-28 18:27:17 +0000

    Adding missing request source constructor files.
    Signed-off-by: William Juan <66322422+wjuan-AFK@users.noreply.github.com>

---
+++ test/mocks/common/mock_request_source_constructor.cc
@@ -0,0 +1,7 @@
+#include "test/mocks/common/mock_request_source_constructor.h"
+
+namespace Nighthawk {
+
+MockRequestSourceConstructor::MockRequestSourceConstructor() = default;
+
+} // namespace Nighthawk
