45e4a4f71f06c1152578cab4d2a9361f03ee3d7e
eric846 <56563761+eric846@users.noreply.github.com>
2020-08-11 15:42:52 -0400

    fix comment
    Signed-off-by: eric846 <56563761+eric846@users.noreply.github.com>

--- include/nighthawk/adaptive_load/config_validator.h
+++ include/nighthawk/adaptive_load/config_validator.h
@@ -9,18 +9,18 @@ namespace Nighthawk {
 /**
  * Interface implemented by plugin config factories to perform proto-specific validations.
  *
- * If a plugin's config proto contains any TypedExtensionConfig fields, the plugin's
- * ValidateConfig() method should attempt to call Load...Plugin() on each TypedExtensionConfig
- * provided. See plugin_util.h. If all plugin config factories follow this convention, the entire
- * adaptive load session spec will be recursively validated at load time.
- *
  * The default ConfigValidator implementation performs no checks and returns OK.
  */
 class ConfigValidator {
 public:
   virtual ~ConfigValidator() = default;
   /**
-   * Check a config for plugin-specific errors.
+   * Checks a config for plugin-specific errors.
+   *
+   * If the config proto contains any TypedExtensionConfig fields, ValidateConfig() should attempt
+   * to call Load...Plugin() on each TypedExtensionConfig field value. See plugin_util.h. If all
+   * plugin config factories follow this convention, the entire adaptive load session spec will be
+   * recursively validated at load time.
    *
    * @param message The Any config proto taken from the TypedExtensionConfig that activated this
    * plugin, to be checked for validity in plugin-specific ways.
