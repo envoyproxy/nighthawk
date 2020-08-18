d8c4f3437e970f9709742e7d6e8cadde35db508c
eric846 <56563761+eric846@users.noreply.github.com>
2020-08-18 02:20:09 -0400

    fix include path
    Signed-off-by: eric846 <56563761+eric846@users.noreply.github.com>

--- test/adaptive_load/fake_plugins/fake_plugins/fake_metrics_plugin/fake_metrics_plugin_test.cc
+++ test/adaptive_load/fake_plugins/fake_plugins/fake_metrics_plugin/fake_metrics_plugin_test.cc
@@ -1,7 +1,7 @@
 #include "api/adaptive_load/benchmark_result.pb.h"
 #include "api/client/options.pb.h"
 #include "envoy/registry/registry.h"
-#include "adaptive_load/plugin_loader.h"
+#include "source/adaptive_load/_virtual_includes/plugin_util/adaptive_load/plugin_loader.h"
 #include "test/adaptive_load/fake_plugins/fake_metrics_plugin/fake_metrics_plugin.h"
 
 #include "external/envoy/source/common/config/utility.h"
