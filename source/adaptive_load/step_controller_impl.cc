80d2e29e4da93ecf893f8be7f3c840f7e6992877
eric846 <56563761+eric846@users.noreply.github.com>
2020-08-18 03:11:44 -0400

    initial commit
    Signed-off-by: eric846 <56563761+eric846@users.noreply.github.com>

---
+++ source/adaptive_load/step_controller_impl.cc
@@ -0,0 +1,161 @@
+#include "adaptive_load/step_controller_impl.h"
+
+#include <memory>
+
+#include "external/envoy/source/common/protobuf/protobuf.h"
+
+#include "api/adaptive_load/adaptive_load.pb.h"
+#include "api/adaptive_load/benchmark_result.pb.h"
+#include "api/adaptive_load/metric_spec.pb.h"
+#include "api/adaptive_load/step_controller_impl.pb.h"
+
+#include "adaptive_load/input_variable_setter_impl.h"
+#include "adaptive_load/plugin_loader.h"
+
+namespace Nighthawk {
+
+namespace {
+
+using nighthawk::adaptive_load::BenchmarkResult;
+using nighthawk::adaptive_load::ExponentialSearchStepControllerConfig;
+using nighthawk::adaptive_load::MetricEvaluation;
+
+/**
+ * Checks if any non-informational metrics (weight > 0) were outside thresholds (score < 0).
+ *
+ * @param benchmark_result Metrics from the latest Nighthawk benchmark session.
+ *
+ * @return double -1.0 if any metric was outside its threshold or 1.0 if all metrics were within
+ * thresholds.
+ */
+double TotalScore(const BenchmarkResult& benchmark_result) {
+  for (const MetricEvaluation& evaluation : benchmark_result.metric_evaluations()) {
+    if (evaluation.weight() > 0.0 && evaluation.threshold_score() < 0.0) {
+      return -1.0;
+    }
+  }
+  return 1.0;
+}
+
+} // namespace
+
+Envoy::ProtobufTypes::MessagePtr
+ExponentialSearchStepControllerConfigFactory::createEmptyConfigProto() {
+  return std::make_unique<ExponentialSearchStepControllerConfig>();
+}
+
+std::string ExponentialSearchStepControllerConfigFactory::name() const {
+  return "nighthawk.exponential-search";
+}
+
+absl::Status ExponentialSearchStepControllerConfigFactory::ValidateConfig(
+    const Envoy::Protobuf::Message& message) const {
+  const auto& any = dynamic_cast<const Envoy::ProtobufWkt::Any&>(message);
+  ExponentialSearchStepControllerConfig config;
+  Envoy::MessageUtil::unpackTo(any, config);
+  if (config.has_input_variable_setter()) {
+    absl::StatusOr<InputVariableSetterPtr> input_variable_setter_or = LoadInputVariableSetterPlugin(config.input_variable_setter());
+    if (!input_variable_setter_or.ok()) {
+      return input_variable_setter_or.status();
+    }
+  }
+  return absl::OkStatus();
+}
+
+StepControllerPtr ExponentialSearchStepControllerConfigFactory::createStepController(
+    const Envoy::Protobuf::Message& message,
+    const nighthawk::client::CommandLineOptions& command_line_options_template) {
+  const auto& any = dynamic_cast<const Envoy::ProtobufWkt::Any&>(message);
+  ExponentialSearchStepControllerConfig config;
+  Envoy::MessageUtil::unpackTo(any, config);
+  return std::make_unique<ExponentialSearchStepController>(config, command_line_options_template);
+}
+
+REGISTER_FACTORY(ExponentialSearchStepControllerConfigFactory, StepControllerConfigFactory);
+
+ExponentialSearchStepController::ExponentialSearchStepController(
+    const ExponentialSearchStepControllerConfig& config,
+    const nighthawk::client::CommandLineOptions& command_line_options_template)
+    : command_line_options_template_{command_line_options_template}, is_exponential_phase_{true},
+      exponential_factor_{config.exponential_factor() > 0.0 ? config.exponential_factor() : 2.0},
+      previous_load_value_{std::numeric_limits<double>::signaling_NaN()},
+      current_load_value_{config.initial_value()},
+      bottom_load_value_{std::numeric_limits<double>::signaling_NaN()},
+      top_load_value_{std::numeric_limits<double>::signaling_NaN()} {
+  doom_reason_ = "";
+  if (config.has_input_variable_setter()) {
+    // Load failures should have been caught during input validation.
+    input_variable_setter_ = LoadInputVariableSetterPlugin(config.input_variable_setter()).value();
+  } else {
+    input_variable_setter_ = std::make_unique<RequestsPerSecondInputVariableSetter>(
+        nighthawk::adaptive_load::RequestsPerSecondInputVariableSetterConfig());
+  }
+}
+
+absl::StatusOr<nighthawk::client::CommandLineOptions>
+ExponentialSearchStepController::GetCurrentCommandLineOptions() const {
+  nighthawk::client::CommandLineOptions options = command_line_options_template_;
+  absl::Status status = input_variable_setter_->SetInputVariable(options, current_load_value_);
+  if (!status.ok()) {
+    return status;
+  }
+  return options;
+}
+
+bool ExponentialSearchStepController::IsConverged() const {
+  // Binary search has brought successive input values within 1% of each other.
+  return doom_reason_.empty() && !is_exponential_phase_ &&
+         abs(current_load_value_ / previous_load_value_ - 1.0) < 0.01;
+}
+
+bool ExponentialSearchStepController::IsDoomed(std::string& doom_reason) const {
+  if (doom_reason_.empty()) {
+    return false;
+  }
+  doom_reason = doom_reason_;
+  return true;
+}
+
+void ExponentialSearchStepController::UpdateAndRecompute(const BenchmarkResult& benchmark_result) {
+  if (benchmark_result.status().code()) {
+    doom_reason_ = "Nighthawk Service returned an error.";
+    return;
+  }
+  const double score = TotalScore(benchmark_result);
+
+  if (is_exponential_phase_) {
+    if (score > 0.0) {
+      // Have not reached the threshold yet; continue increasing the load exponentially.
+      previous_load_value_ = current_load_value_;
+      current_load_value_ *= exponential_factor_;
+    } else {
+      // We have found a value that exceeded the threshold.
+      // Prepare for the binary search phase.
+      if (std::isnan(previous_load_value_)) {
+        // Cannot continue if the initial value already exceeds metric thresholds.
+        doom_reason_ = "Outside threshold on initial input.";
+        return;
+      }
+      is_exponential_phase_ = false;
+      // Binary search between previous load (ok) and current load (too high).
+      bottom_load_value_ = previous_load_value_;
+      top_load_value_ = current_load_value_;
+
+      previous_load_value_ = current_load_value_;
+      current_load_value_ = (bottom_load_value_ + top_load_value_) / 2;
+    }
+  } else {
+    // Binary search phase.
+    if (score > 0.0) {
+      // Within threshold, go higher.
+      bottom_load_value_ = current_load_value_;
+    } else {
+      // Outside threshold, go lower.
+      top_load_value_ = current_load_value_;
+    }
+    previous_load_value_ = current_load_value_;
+    current_load_value_ = (bottom_load_value_ + top_load_value_) / 2;
+  }
+}
+
+} // namespace Nighthawk
---
+++ test/adaptive_load/step_controller_test.cc
@@ -0,0 +1,228 @@
+#include "envoy/registry/registry.h"
+
+#include "nighthawk/adaptive_load/input_variable_setter.h"
+
+#include "external/envoy/source/common/config/utility.h"
+
+#include "api/adaptive_load/benchmark_result.pb.h"
+#include "api/adaptive_load/input_variable_setter_impl.pb.h"
+#include "api/adaptive_load/step_controller_impl.pb.h"
+#include "api/client/options.pb.h"
+
+#include "adaptive_load/plugin_loader.h"
+#include "adaptive_load/config_validator_impl.h"
+#include "adaptive_load/step_controller_impl.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace Nighthawk {
+
+namespace {
+
+nighthawk::adaptive_load::BenchmarkResult MakeBenchmarkResultOutsideThreshold() {
+  nighthawk::adaptive_load::BenchmarkResult result;
+  nighthawk::adaptive_load::MetricEvaluation* evaluation = result.add_metric_evaluations();
+  evaluation->set_threshold_score(-1.0);
+  evaluation->set_weight(10.0);
+  return result;
+}
+
+nighthawk::adaptive_load::BenchmarkResult MakeBenchmarkResultWithinThreshold() {
+  nighthawk::adaptive_load::BenchmarkResult result;
+  nighthawk::adaptive_load::MetricEvaluation* evaluation = result.add_metric_evaluations();
+  evaluation->set_threshold_score(1.0);
+  evaluation->set_weight(10.0);
+  return result;
+}
+
+nighthawk::adaptive_load::BenchmarkResult MakeBenchmarkResultWithNighthawkError() {
+  nighthawk::adaptive_load::BenchmarkResult result;
+  result.mutable_status()->set_code(1);
+  return result;
+}
+
+/**
+ * Non-default InputVariableSetter for testing.
+ */
+class ConnectionsInputVariableSetter : public InputVariableSetter {
+public:
+  ConnectionsInputVariableSetter() {}
+  absl::Status SetInputVariable(nighthawk::client::CommandLineOptions& command_line_options,
+                                double input_value) override {
+    command_line_options.mutable_connections()->set_value(static_cast<unsigned int>(input_value));
+    return absl::OkStatus();
+  }
+};
+
+/**
+ * A factory that creates a ConnectionsInputVariableSetter from a
+ * ConnectionsInputVariableSetterConfig proto.
+ */
+class ConnectionsInputVariableSetterConfigFactory : public InputVariableSetterConfigFactory, public virtual NullConfigValidator {
+public:
+  std::string name() const override { return "nighthawk.testing-connections"; }
+  Envoy::ProtobufTypes::MessagePtr createEmptyConfigProto() override {
+    return std::make_unique<Envoy::ProtobufWkt::Any>();
+  }
+  InputVariableSetterPtr createInputVariableSetter(const Envoy::Protobuf::Message&) override {
+    return std::make_unique<ConnectionsInputVariableSetter>();
+  }
+};
+
+REGISTER_FACTORY(ConnectionsInputVariableSetterConfigFactory, InputVariableSetterConfigFactory);
+
+TEST(ExponentialSearchStepControllerConfigFactory, GeneratesEmptyConfigProto) {
+  StepControllerConfigFactory& config_factory =
+      Envoy::Config::Utility::getAndCheckFactoryByName<StepControllerConfigFactory>(
+          "nighthawk.exponential-search");
+  Envoy::ProtobufTypes::MessagePtr message = config_factory.createEmptyConfigProto();
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig expected_config;
+  EXPECT_EQ(message->DebugString(), expected_config.DebugString());
+}
+
+TEST(ExponentialSearchStepControllerConfigFactory, CreatesCorrectFactoryName) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  Envoy::ProtobufWkt::Any config_any;
+  config_any.PackFrom(config);
+  nighthawk::client::CommandLineOptions options;
+  StepControllerConfigFactory& config_factory =
+      Envoy::Config::Utility::getAndCheckFactoryByName<StepControllerConfigFactory>(
+          "nighthawk.exponential-search");
+  EXPECT_EQ(config_factory.name(), "nighthawk.exponential-search");
+}
+
+TEST(ExponentialSearchStepControllerConfigFactory, CreatesCorrectPluginType) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  Envoy::ProtobufWkt::Any config_any;
+  config_any.PackFrom(config);
+  nighthawk::client::CommandLineOptions options;
+  StepControllerConfigFactory& config_factory =
+      Envoy::Config::Utility::getAndCheckFactoryByName<StepControllerConfigFactory>(
+          "nighthawk.exponential-search");
+  StepControllerPtr plugin = config_factory.createStepController(config_any, options);
+  EXPECT_NE(dynamic_cast<ExponentialSearchStepController*>(plugin.get()), nullptr);
+}
+
+TEST(ExponentialSearchStepController, UsesInitialRps) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  EXPECT_EQ(step_controller.GetCurrentCommandLineOptions().value().requests_per_second().value(), 100);
+}
+
+TEST(ExponentialSearchStepController, ActivatesCustomInputValueSetter) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig step_controller_config;
+  step_controller_config.mutable_input_variable_setter()->set_name("nighthawk.testing-connections");
+  // typed_config can be set to any valid Any proto, as the test-only
+  // ConnectionsInputVariableSetterConfigFactory defined above ignores the config proto.
+  *step_controller_config.mutable_input_variable_setter()->mutable_typed_config() =
+      Envoy::ProtobufWkt::Any();
+  step_controller_config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(step_controller_config, options_template);
+  EXPECT_EQ(step_controller.GetCurrentCommandLineOptions().value().connections().value(), 100);
+}
+
+TEST(ExponentialSearchStepController, InitiallyReportsNotConverged) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  EXPECT_FALSE(step_controller.IsConverged());
+}
+
+TEST(ExponentialSearchStepController, InitiallyReportsNotDoomed) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  std::string doom_reason = "untouched";
+  EXPECT_FALSE(step_controller.IsDoomed(doom_reason));
+  EXPECT_EQ(doom_reason, "untouched");
+}
+
+TEST(ExponentialSearchStepController, ReportsDoomIfOutsideThresholdsOnInitialValue) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  // Initial RPS already put us outside metric thresholds.
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultOutsideThreshold());
+  std::string doom_reason;
+  EXPECT_TRUE(step_controller.IsDoomed(doom_reason));
+  EXPECT_EQ(doom_reason, "Outside threshold on initial input.");
+}
+
+TEST(ExponentialSearchStepController, ReportsDoomAfterNighthawkServiceError) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultWithNighthawkError());
+  std::string doom_reason;
+  EXPECT_TRUE(step_controller.IsDoomed(doom_reason));
+  EXPECT_EQ(doom_reason, "Nighthawk Service returned an error.");
+}
+
+TEST(ExponentialSearchStepController,
+     IncreasesRpsExponentiallyIfWithinThresholdUsingDefaultExponent) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultWithinThreshold());
+  // Default exponent is 2.0.
+  EXPECT_EQ(step_controller.GetCurrentCommandLineOptions().value().requests_per_second().value(), 200);
+}
+
+TEST(ExponentialSearchStepController,
+     IncreasesRpsExponentiallyIfWithinThresholdUsingCustomExponent) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  config.set_exponential_factor(1.5);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultWithinThreshold());
+  EXPECT_EQ(step_controller.GetCurrentCommandLineOptions().value().requests_per_second().value(), 150);
+}
+
+TEST(ExponentialSearchStepController, PerformsBinarySearchAfterExceedingThreshold) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultWithinThreshold());
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultOutsideThreshold());
+  EXPECT_EQ(step_controller.GetCurrentCommandLineOptions().value().requests_per_second().value(), 150);
+}
+
+TEST(ExponentialSearchStepController, BinarySearchConvergesAfterManySteps) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultWithinThreshold());
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultOutsideThreshold());
+  for (int i = 0; i < 100; ++i) {
+    step_controller.UpdateAndRecompute(MakeBenchmarkResultOutsideThreshold());
+  }
+  EXPECT_TRUE(step_controller.IsConverged());
+}
+
+TEST(ExponentialSearchStepController, BinarySearchFindsBottomOfRange) {
+  nighthawk::adaptive_load::ExponentialSearchStepControllerConfig config;
+  config.set_initial_value(100.0);
+  nighthawk::client::CommandLineOptions options_template;
+  ExponentialSearchStepController step_controller(config, options_template);
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultWithinThreshold());
+  step_controller.UpdateAndRecompute(MakeBenchmarkResultOutsideThreshold());
+  for (int i = 0; i < 100; ++i) {
+    step_controller.UpdateAndRecompute(MakeBenchmarkResultOutsideThreshold());
+  }
+  EXPECT_EQ(step_controller.GetCurrentCommandLineOptions().value().requests_per_second().value(), 100);
+}
+
+} // namespace
+
+} // namespace Nighthawk
