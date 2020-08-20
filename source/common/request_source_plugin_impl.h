0b25261f686c7d6096c4354aa03e7393fa6e13b5
William Juan <66322422+wjuan-AFK@users.noreply.github.com>
2020-08-20 19:28:56 +0000

    Adding more factories to the impl.h
    Signed-off-by: William Juan <66322422+wjuan-AFK@users.noreply.github.com>

--- source/common/request_source_plugin_impl.h
+++ source/common/request_source_plugin_impl.h
@@ -40,4 +40,69 @@ public:
 
 // This factory is activated through ???.
 DECLARE_FACTORY(DummyRequestSourceConfigFactory);
+
+/**
+ */
+class RPCRequestSourcePlugin : public RequestSourcePlugin {
+public:
+  explicit RPCRequestSourcePlugin(
+      const nighthawk::request_source::RPCPluginRequestSourceConfig& config);
+  RequestGenerator get() override;
+  /**
+   * Will be called on an intialized and running worker thread, before commencing actual work.
+   * Can be used to prepare the request source implementation (opening any connection or files
+   * needed, for example).
+   */
+  void initOnThread() override;
+
+private:
+  const std::string uri_;
+};
+
+/**
+ * Registered as an Envoy plugin.
+ */
+class RPCRequestSourceConfigFactory : public virtual RequestSourcePluginConfigFactory
+{
+public:
+  std::string name() const override;
+  Envoy::ProtobufTypes::MessagePtr createEmptyConfigProto() override;
+  RequestSourcePluginPtr createRequestSourcePlugin(const Envoy::Protobuf::Message& message) override;
+};
+
+// This factory is activated through ???.
+DECLARE_FACTORY(RPCRequestSourceConfigFactory);
+
+/**
+ */
+class FileBasedRequestSourcePlugin : public RequestSourcePlugin {
+public:
+  explicit FileBasedRequestSourcePlugin(
+      const nighthawk::request_source::FileBasedPluginRequestSourceConfig& config);
+  RequestGenerator get() override;
+  /**
+   * Will be called on an intialized and running worker thread, before commencing actual work.
+   * Can be used to prepare the request source implementation (opening any connection or files
+   * needed, for example).
+   */
+  void initOnThread() override;
+
+private:
+  const std::string uri_;
+};
+
+/**
+ * Registered as an Envoy plugin.
+ */
+class FileBasedRequestSourceConfigFactory : public virtual RequestSourcePluginConfigFactory
+{
+public:
+  std::string name() const override;
+  Envoy::ProtobufTypes::MessagePtr createEmptyConfigProto() override;
+  RequestSourcePluginPtr createRequestSourcePlugin(const Envoy::Protobuf::Message& message) override;
+};
+
+// This factory is activated through ???.
+DECLARE_FACTORY(FileBasedRequestSourceConfigFactory);
+
 }
