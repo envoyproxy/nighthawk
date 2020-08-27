78ed3b70a65adea8b238bc47ab342982d9962e39
eric846 <56563761+eric846@users.noreply.github.com>
2020-08-26 22:54:13 -0400

    initial commit - fake time source
    Signed-off-by: eric846 <56563761+eric846@users.noreply.github.com>

---
+++ test/adaptive_load/fake_time_source.cc
@@ -0,0 +1,17 @@
+#include "test/adaptive_load/fake_time_source.h"
+
+namespace Nighthawk {
+
+Envoy::SystemTime FakeIncrementingMonotonicTimeSource::systemTime() {
+  Envoy::SystemTime epoch;
+  return epoch;
+}
+
+Envoy::MonotonicTime FakeIncrementingMonotonicTimeSource::monotonicTime() {
+  Envoy::MonotonicTime epoch;
+  Envoy::MonotonicTime result = epoch + std::chrono::seconds(seconds_since_epoch_);
+  ++seconds_since_epoch_;
+  return result;
+}
+
+} // namespace Nighthawk
---
+++ test/adaptive_load/fake_time_source.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "envoy/common/time.h"
+
+namespace Nighthawk {
+/**
+ * Fake time source that ticks 1 second on every query, starting from the Unix epoch. Supports only
+ * monotonicTime().
+ */
+class FakeIncrementingMonotonicTimeSource : public Envoy::TimeSource {
+public:
+  /**
+   * Not supported.
+   *
+   * @return Envoy::SystemTime Fixed value of the Unix epoch.
+   */
+  Envoy::SystemTime systemTime() override;
+  /**
+   * Ticks forward 1 second on each call.
+   *
+   * @return Envoy::MonotonicTime Fake time value.
+   */
+  Envoy::MonotonicTime monotonicTime() override;
+
+private:
+  int seconds_since_epoch_{0};
+};
+
+} // namespace Nighthawk
