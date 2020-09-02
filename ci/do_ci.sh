8317e68dff8d57b3c3e1ae385cc1d7fc63d0915e
Otto van der Schaaf <oschaaf@we-amp.com>
2020-09-02 15:54:35 +0200

    Amend coverage thresholds
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -34,7 +34,7 @@ function do_clang_tidy() {
 
 function do_unit_test_coverage() {
     export TEST_TARGETS="//test/... -//test:python_test"
-    export COVERAGE_THRESHOLD=93.3
+    export COVERAGE_THRESHOLD=93.2
     echo "bazel coverage build with tests ${TEST_TARGETS}"
     test/run_nighthawk_bazel_coverage.sh ${TEST_TARGETS}
     exit 0
@@ -42,7 +42,7 @@ function do_unit_test_coverage() {
 
 function do_integration_test_coverage() {
     export TEST_TARGETS="//test:python_test"
-    export COVERAGE_THRESHOLD=78.2
+    export COVERAGE_THRESHOLD=78.1
     echo "bazel coverage build with tests ${TEST_TARGETS}"
     test/run_nighthawk_bazel_coverage.sh ${TEST_TARGETS}
     exit 0
