f263b5283c743b5bf11d14d73423d75181d40768
Otto van der Schaaf <oschaaf@we-amp.com>
2020-09-22 12:49:03 +0200

    Implement RemoteProcessImpl in terms of PerformNighthawkBenchmark()
    Drop duplicated code, use the newer code that has better test
    coverage plus handling for when multiple results would be received.
    
    Fixes #496
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/client/remote_process_impl.cc
+++ source/client/remote_process_impl.cc
@@ -9,6 +9,7 @@
 #include "api/client/options.pb.h"
 #include "api/client/output.pb.h"
 
+#include "common/nighthawk_service_client_impl.h"
 #include "common/uri_impl.h"
 
 #include "client/options_impl.h"
@@ -18,39 +19,23 @@ namespace Client {
 
 RemoteProcessImpl::RemoteProcessImpl(const Options& options,
                                      nighthawk::client::NighthawkService::Stub& stub)
-    : options_(options), stub_(stub) {}
+    : options_(options), service_client_(std::make_unique<NighthawkServiceClientImpl>()),
+      stub_(stub) {}
 
 bool RemoteProcessImpl::run(OutputCollector& collector) {
-  nighthawk::client::ExecutionRequest request;
-  nighthawk::client::ExecutionResponse response;
-  grpc::ClientContext context;
-  auto execution_stream = stub_.ExecutionStream(&context);
-
-  *request.mutable_start_request()->mutable_options() = *options_.toCommandLineOptions();
+  Nighthawk::Client::CommandLineOptionsPtr options = options_.toCommandLineOptions();
   // We don't forward the option that requests remote execution. Today,
   // nighthawk_service will ignore the option, but if someone ever changes that this
   // is probably desireable.
-  request.mutable_start_request()->mutable_options()->mutable_nighthawk_service()->Clear();
+  options->mutable_nighthawk_service()->Clear();
 
-  if (execution_stream->Write(request, {}) && execution_stream->Read(&response)) {
-    if (response.has_output()) {
-      collector.setOutput(response.output());
-    } else {
-      ENVOY_LOG(error, "Remote execution failed");
-    }
-    if (response.has_error_detail()) {
-      ENVOY_LOG(error, "have error detail: {}", response.error_detail().DebugString());
-    }
-    if (!execution_stream->WritesDone()) {
-      ENVOY_LOG(warn, "writeDone() failed");
-    } else {
-      auto status = execution_stream->Finish();
-      return status.ok();
-    }
-  } else {
-    ENVOY_LOG(error, "Failure while communicating with the remote service");
+  const absl::StatusOr<const nighthawk::client::ExecutionResponse> result =
+      service_client_->PerformNighthawkBenchmark(&stub_, *options);
+  if (result.ok()) {
+    collector.setOutput(result.value().output());
+    return true;
   }
-
+  ENVOY_LOG(error, "Remote execution failure: {}", result.status().message());
   return false;
 }
 
