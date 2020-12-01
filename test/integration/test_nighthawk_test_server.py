0e3d4998c8f32428af2f5cb23cf38fc76003cd43
Jakub Sobon <mumak@google.com>
2020-12-01 00:23:00 -0500

    Autoformatting Python files.
    Signed-off-by: Jakub Sobon <mumak@google.com>

--- test/integration/test_nighthawk_test_server.py
+++ test/integration/test_nighthawk_test_server.py
@@ -17,8 +17,7 @@ def test_extractWarningsAndErrors_ignores_info_logs():
   process_output = """
   [2020-12-01 04:41:57.219][126][info][misc] Message.
   """
-  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(
-      process_output, [])
+  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(process_output, [])
   assert not warnings
   assert not errors
 
@@ -26,8 +25,7 @@ def test_extractWarningsAndErrors_ignores_info_logs():
 def test_extractWarningsAndErrors_extracts_a_warning():
   """Test where the process output contains a single warning."""
   process_output = "[2020-12-01 04:41:57.219][126][warning][misc] Message."
-  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(
-      process_output, [])
+  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(process_output, [])
   assert warnings == ["[2020-12-01 04:41:57.219][126][warning][misc] Message."]
   assert not errors
 
@@ -35,8 +33,7 @@ def test_extractWarningsAndErrors_extracts_a_warning():
 def test_extractWarningsAndErrors_extracts_an_error():
   """Test where the process output contains a single error."""
   process_output = "[2020-12-01 04:41:57.219][126][error][misc] Message."
-  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(
-      process_output, [])
+  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(process_output, [])
   assert not warnings
   assert errors == ["[2020-12-01 04:41:57.219][126][error][misc] Message."]
 
@@ -49,11 +46,8 @@ def test_extractWarningsAndErrors_extracts_multiple_messages():
 [error][runtime] Error2.
 [warning][runtime] Warning2.
   """
-  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(
-      process_output, [])
-  assert warnings == [
-      "[warning][misc] Warning1.", "[warning][runtime] Warning2."
-  ]
+  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(process_output, [])
+  assert warnings == ["[warning][misc] Warning1.", "[warning][runtime] Warning2."]
   assert errors == ["[error][misc] Error1.", "[error][runtime] Error2."]
 
 
@@ -67,8 +61,7 @@ def test_extractWarningsAndErrors_skips_messages_matching_ignore_list():
   """
 
   ignore_list = ["foo", "bar"]
-  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(
-      process_output, ignore_list)
+  warnings, errors = nighthawk_test_server._extractWarningsAndErrors(process_output, ignore_list)
   assert not warnings
   assert errors == ["[error][runtime] Error2 baz."]
 
