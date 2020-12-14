3f9d6c269cae8404106b04b74b7d85232603a040
Otto van der Schaaf <oschaaf@we-amp.com>
2020-12-14 03:58:18 +0100

    Update Envoy to 424909395c90d7d68f1afeb3427c26c7c85f2672 (#595)
    - The way we can obtain a http connection pool changed. Amend.
    - Cleanup by fix_format: strip superfluous .Times(1) in tests.
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/stream_decoder_test.cc
+++ test/stream_decoder_test.cc
@@ -136,9 +136,9 @@ TEST_F(StreamDecoderTest, LatencyIsMeasured) {
                      const Envoy::Tracing::Decision) -> Envoy::Tracing::Span* {
             EXPECT_EQ(Envoy::Tracing::OperationName::Egress, config.operationName());
             auto* span = new Envoy::Tracing::MockSpan();
-            EXPECT_CALL(*span, injectContext(_)).Times(1);
+            EXPECT_CALL(*span, injectContext(_));
             EXPECT_CALL(*span, setTag(_, _)).Times(12);
-            EXPECT_CALL(*span, finishSpan()).Times(1);
+            EXPECT_CALL(*span, finishSpan());
             return span;
           }));
 
