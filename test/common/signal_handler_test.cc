3b2107f980043aa84ce47283191fc7f3cb5d7a91
Otto van der Schaaf <oschaaf@we-amp.com>
2021-01-06 00:21:59 +0100

    Signal handler: don't fire on destruction
    Fixes a bug observed while working on horizontal scaling support:
    SignalHandler would fire on destruction.
    Fix & add tests.
    
    Note: pre-emptive, I don't think this is  an issue in the current
    state of master. But some of the CI errors in #600 seemed familiar
    and hopefully merging this in there helps.
    
    (Split out from https://github.com/oschaaf/nighthawk/tree/horizontal-scaling)
    
    /cc @eric846
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

---
+++ test/common/signal_handler_test.cc
@@ -0,0 +1,35 @@
+#include <csignal>
+#include <future>
+
+#include "common/signal_handler.h"
+
+#include "gtest/gtest.h"
+
+namespace Nighthawk {
+namespace {
+
+TEST(SignalHandlerTest, SignalGetsHandled) {
+  for (const auto signal : {SIGTERM, SIGINT}) {
+    bool signal_handled = false;
+    std::promise<void> signal_all_threads_running;
+
+    SignalHandler signal_handler([&signal_handled, &signal_all_threads_running]() {
+      signal_handled = true;
+      signal_all_threads_running.set_value();
+    });
+    std::raise(signal);
+    signal_all_threads_running.get_future().wait();
+    EXPECT_TRUE(signal_handled);
+  }
+}
+
+TEST(SignalHandlerTest, DeststructDoesNotFireHandler) {
+  bool signal_handled = false;
+  {
+    SignalHandler signal_handler([&signal_handled]() { signal_handled = true; });
+  }
+  EXPECT_FALSE(signal_handled);
+}
+
+} // namespace
+} // namespace Nighthawk
