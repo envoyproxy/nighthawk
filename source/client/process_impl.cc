71b0433c6a4c0c6aa840cff58c533e96ec27b711
eric846 <56563761+eric846@users.noreply.github.com>
2021-02-11 16:38:00 -0500

    add more localhost advice
    Signed-off-by: eric846 <56563761+eric846@users.noreply.github.com>

--- source/client/process_impl.cc
+++ source/client/process_impl.cc
@@ -621,11 +621,11 @@ absl::Status ProcessImpl::runInternal(OutputCollector& collector, const std::vec
         "values. The default Nighthawk failure predicates report failure if (1) Nighthawk could "
         "not connect to the target (see 'benchmark.pool_connection_failure' counter; check the "
         "address and port number, and try explicitly setting --address-family v4 or v6, especially "
-        "when using 'localhost' or DNS), (2) the protocol was not supported by the target (see "
-        "'benchmark.stream_reset' counter; check http/https in the URI, --h2), (3) the target "
-        "returned a 4xx or 5xx HTTP response code (see 'benchmark.http_#xx' counters; check the "
-        "URI path and the server config), or (4) a custom gRPC RequestSource failed. "
-        "--failure-predicate can be used to relax expectations.");
+        "when using DNS; instead of localhost try 127.0.0.1 or ::1 explicitly), (2) the protocol "
+        "was not supported by the target (see 'benchmark.stream_reset' counter; check http/https "
+        "in the URI, --h2), (3) the target returned a 4xx or 5xx HTTP response code (see "
+        "'benchmark.http_#xx' counters; check the URI path and the server config), or (4) a custom "
+        "gRPC RequestSource failed. --failure-predicate can be used to relax expectations.");
   }
   return absl::OkStatus();
 }
