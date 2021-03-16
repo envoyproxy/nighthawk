b5a8515343732df422eb01bc4cd04a1303d4fb6d
Otto van der Schaaf <ovanders@redhat.com>
2021-03-16 22:51:44 +0100

    Clean up deps, doc interface.
    Signed-off-by: Otto van der Schaaf <ovanders@redhat.com>

--- include/nighthawk/distributor/nighthawk_distributor_client.h
+++ include/nighthawk/distributor/nighthawk_distributor_client.h
@@ -8,10 +8,21 @@
 
 namespace Nighthawk {
 
+/**
+ * Interface of a gRPC distributor service client.
+ */
 class NighthawkDistributorClient {
 public:
   virtual ~NighthawkDistributorClient() = default;
 
+  /**
+   * @brief Propagate messages to one or more other services for handling.
+   *
+   * @param nighthawk_distributor_stub Used to open a channel to the distributor service.
+   * @param distributed_request Provide the message that the distributor service should propagate.
+   * @return absl::StatusOr<::nighthawk::DistributedResponse> Either a status indicating failure, or
+   * a DistributedResponse upon success.
+   */
   virtual absl::StatusOr<::nighthawk::DistributedResponse>
   DistributedRequest(nighthawk::NighthawkDistributor::StubInterface& nighthawk_distributor_stub,
                      const nighthawk::DistributedRequest& distributed_request) const PURE;
--- source/distributor/nighthawk_distributor_client_impl.h
+++ source/distributor/nighthawk_distributor_client_impl.h
@@ -1,11 +1,5 @@
 #include "nighthawk/common/nighthawk_distributor_client.h"
 
-#include "external/envoy/source/common/common/statusor.h"
-#include "external/envoy/source/common/protobuf/protobuf.h"
-
-#include "api/client/options.pb.h"
-#include "api/client/service.grpc.pb.h"
-
 namespace Nighthawk {
 
 class NighthawkDistributorClientImpl : public NighthawkDistributorClient {
