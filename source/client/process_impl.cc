819af069d941b108504486d65f618c5d621ae8d7
Jakub Sobon <mumak@google.com>
2021-08-05 22:41:19 -0400

    Using protocol version instead of a bool h2 when creating the bootstrap.
    Signed-off-by: Jakub Sobon <mumak@google.com>

--- source/client/process_impl.cc
+++ source/client/process_impl.cc
@@ -316,15 +316,16 @@ void ProcessImpl::createBootstrapConfiguration(envoy::config::bootstrap::v3::Boo
       transport_socket->set_name("envoy.transport_sockets.tls");
       envoy::extensions::transport_sockets::tls::v3::UpstreamTlsContext context =
           options_.tlsContext();
-      const std::string sni_host = SniUtility::computeSniHost(
-          uris, options_.requestHeaders(),
-          options_.h2() ? Envoy::Http::Protocol::Http2 : Envoy::Http::Protocol::Http11);
+      const std::string sni_host =
+          SniUtility::computeSniHost(uris, options_.requestHeaders(), options_.upstreamProtocol());
       if (!sni_host.empty()) {
         *context.mutable_sni() = sni_host;
       }
       auto* common_tls_context = context.mutable_common_tls_context();
-      if (options_.h2()) {
+      if (options_.upstreamProtocol() == Envoy::Http::Protocol::Http2) {
         common_tls_context->add_alpn_protocols("h2");
+      } else if (options_.upstreamProtocol() == Envoy::Http::Protocol::Http3) {
+        throw NighthawkException("HTTP/3 Quic support isn't implemented yet.");
       } else {
         common_tls_context->add_alpn_protocols("http/1.1");
       }
@@ -336,11 +337,10 @@ void ProcessImpl::createBootstrapConfiguration(envoy::config::bootstrap::v3::Boo
     cluster->set_name(fmt::format("{}", i));
     cluster->mutable_connect_timeout()->set_seconds(options_.timeout().count());
     cluster->mutable_max_requests_per_connection()->set_value(options_.maxRequestsPerConnection());
-    if (options_.h2()) {
+    if (options_.upstreamProtocol() == Envoy::Http::Protocol::Http2) {
       auto* cluster_http2_protocol_options = cluster->mutable_http2_protocol_options();
-      if (options_.h2UseMultipleConnections()) {
-        cluster_http2_protocol_options->mutable_max_concurrent_streams()->set_value(1);
-      }
+      cluster_http2_protocol_options->mutable_max_concurrent_streams()->set_value(
+          options_.maxConcurrentStreams());
     }
 
     auto thresholds = cluster->mutable_circuit_breakers()->add_thresholds();
