3251ec5574c0ce16e314a7c0f943f90d0b697823
Jakub Sobon <mumak@google.com>
2021-08-31 14:54:16 -0400

    Update Envoy to 74d5ef8 (Aug 30th 2021). (#740)
    - renamed `Envoy::Network::SocketAddressProvider` to `Envoy::Network::ConnectionInfoProvider` as per
      https://github.com/envoyproxy/envoy/pull/17717.
    - removed a boolean argument in a call to `Envoy::MessageUtil::loadFromFile` as per https://github.com/envoyproxy/envoy/pull/17724.
    - removing reference to and presence of the unknown proto field `Envoy::ProtobufWellKnown::OriginalTypeFieldNumber` after Envoy deleted it in https://github.com/envoyproxy/envoy/pull/17724.
    - updated the command line in `README.md` to reflect Envoy's removal of flags related to bootstrap version.
    - moving Envoy configurations used in integration tests off the deprecated field `envoy.config.bootstrap.v3.Admin.access_log_path`.
    - no changes to `.bazelrc`, `.bazelversion`, `run_envoy_docker.sh`.
    
    Signed-off-by: Jakub Sobon <mumak@google.com>

--- test/test_common/proto_matchers.h
+++ test/test_common/proto_matchers.h
@@ -1,41 +1,11 @@
 #include <string>
 
 #include "external/envoy/source/common/protobuf/protobuf.h"
-#include "external/envoy/source/common/protobuf/well_known.h"
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace Nighthawk {
-
-// A custom IgnoreCriteria that can be added to a MessageDifferencer to ignore
-// unknown fields by their field number, regardless of where they appear in a
-// message.
-class IgnoreUnknownFieldsGloballyByNumber
-    : public Envoy::Protobuf::util::MessageDifferencer::IgnoreCriteria {
-public:
-  // Constructs an ignore criteria instance that will ignore differences in all
-  // unknown proto fields whose field number matches the one specified.
-  explicit IgnoreUnknownFieldsGloballyByNumber(int ignored_field_number);
-
-  // Does not ignore any fields on this implementation.
-  // Implemented only to satisfy the interface.
-  bool IsIgnored(const Envoy::Protobuf::Message& message1, const Envoy::Protobuf::Message& message2,
-                 const Envoy::Protobuf::FieldDescriptor* field,
-                 const std::vector<Envoy::Protobuf::util::MessageDifferencer::SpecificField>&
-                     parent_fields) override;
-
-  // Ignores an unknown field if its field number equals to the one provided to the
-  // constructor.
-  bool IsUnknownFieldIgnored(
-      const Envoy::Protobuf::Message& message1, const Envoy::Protobuf::Message& message2,
-      const Envoy::Protobuf::util::MessageDifferencer::SpecificField& field,
-      const std::vector<Envoy::Protobuf::util::MessageDifferencer::SpecificField>& parent_fields)
-      override;
-
-private:
-  const int ignored_field_number_;
-};
 
 // Compares two proto messages for equality.
 // Prints diff on failures.
@@ -49,11 +19,6 @@ MATCHER_P(EqualsProto, expected_proto, "is equal to the expected_proto") {
   std::string diff;
   Envoy::Protobuf::util::MessageDifferencer differ;
   differ.ReportDifferencesToString(&diff);
-
-  // Envoy proto messages have a well known field with this number that needs to
-  // be ignored in proto comparisons.
-  differ.AddIgnoreCriteria(
-      new IgnoreUnknownFieldsGloballyByNumber(Envoy::ProtobufWellKnown::OriginalTypeFieldNumber));
 
   bool equal = differ.Compare(arg, expected_proto);
   if (!equal) {
