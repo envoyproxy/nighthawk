403cbb9886110661def2deef708279ae6ef66710
Andrew Helsley <helsleya@cs.ucr.edu>
2021-09-03 20:32:53 -0700

    test: More MOCK_METHOD modernization. (#742)
    * Use more `MOCK_METHOD` instead of `MOCK_METHODn`.
    * Add the `override` qualifier where appropriate.
    
    This PR is based on some automated changes suggested by @mum4k with tweaks to remove some `override`s that failed to build.
    
    Signed-off-by: Andrew Helsley <ahelsley@google.com>
    Co-authored-by: Jakub Sobon <mumak@google.com>

--- test/mocks/common/mock_termination_predicate_factory.h
+++ test/mocks/common/mock_termination_predicate_factory.h
@@ -9,10 +9,10 @@ namespace Nighthawk {
 class MockTerminationPredicateFactory : public TerminationPredicateFactory {
 public:
   MockTerminationPredicateFactory();
-  MOCK_CONST_METHOD3(create,
-                     TerminationPredicatePtr(Envoy::TimeSource& time_source,
-                                             Envoy::Stats::Scope& scope,
-                                             const Envoy::MonotonicTime scheduled_starting_time));
+  MOCK_METHOD(TerminationPredicatePtr, create,
+              (Envoy::TimeSource & time_source, Envoy::Stats::Scope& scope,
+               const Envoy::MonotonicTime scheduled_starting_time),
+              (const, override));
 };
 
 } // namespace Nighthawk
