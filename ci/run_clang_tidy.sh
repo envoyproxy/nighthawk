65348a35fde44c5036617814ca6cd1fd74beaa28
tomjzzhang <4367421+tomjzzhang@users.noreply.github.com>
2022-05-11 18:47:17 +0000

    Print more information
    Signed-off-by: tomjzzhang <4367421+tomjzzhang@users.noreply.github.com>

--- ci/run_clang_tidy.sh
+++ ci/run_clang_tidy.sh
@@ -92,6 +92,9 @@ function filter_excludes() {
 
 function run_clang_tidy() {
   echo "Running clang_tidy with NUM_CPUS: ${NUM_CPUS:-0}."
+  ls  "${LLVM_PREFIX}/share/clang/"
+  ls  "${LLVM_PREFIX}/share/"
+  uname -m
   python3 "${LLVM_PREFIX}/share/clang/run-clang-tidy.py" \
     -clang-tidy-binary="${CLANG_TIDY}" \
     -clang-apply-replacements-binary="${CLANG_APPLY_REPLACEMENTS}" \
