1e0c354a62676944f178ef036d58305381536444
jiajunye <jiajunye@google.com>
2022-07-14 17:07:15 +0000

    only run the failed test_remote_execution_basics test
    Signed-off-by: jiajunye <jiajunye@google.com>

--- test/python_test.cc
+++ test/python_test.cc
@@ -13,11 +13,14 @@ class PythonTest : public Test {};
 // of getting code coverage reporting to also consider the code hit by integration tests.
 TEST_F(PythonTest, IntegrationTests) {
   const std::string path = TestEnvironment::runfilesPath("test/integration/integration_test");
+  // The binary accepts a python expression it compares all test targets against.
+  // See the k argument in https://docs.pytest.org/en/6.2.x/reference.html#command-line-flags
+  const std::string altered_cmd = path + " test_remote_execution_basics";
 #if defined(__has_feature) && (__has_feature(thread_sanitizer) || __has_feature(address_sanitizer))
   char env[] = "NH_INTEGRATION_TEST_SANITIZER_RUN=1";
   putenv(env);
 #endif
-  ASSERT_EQ(0, system(path.c_str()));
+  ASSERT_EQ(0, system(altered_cmd.c_str()));
 }
 
 } // namespace Nighthawk
