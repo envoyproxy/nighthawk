7ad1cd9fd3a15197e7049604d72f0c67f55f5d5b
Kevin Baichoo <kbaichoo@google.com>
2022-08-23 17:19:08 +0000

    Split benchmarks into static and dynamic tests.
    Signed-off-by: Kevin Baichoo <kbaichoo@google.com>

---
+++ benchmarks/static_benchmarks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""@package benchmarks.
+
+Entry point for benchmark execution.
+"""
+import os
+import sys
+import pytest
+
+if __name__ == '__main__':
+  path = os.path.dirname(os.path.realpath(__file__))
+  r = pytest.main([
+      "--rootdir=" + path, "-x", (path + '/static_test/'), "-p", "no:cacheprovider", "--log-level", "INFO",
+      "--log-cli-level", "INFO", *sys.argv
+  ])
+  exit(r)
