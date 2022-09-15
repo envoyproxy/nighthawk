5a4d792dc5158fbb43a7b2af3973d5a51497d1ce
Nathan Perry <nbperry@google.com>
2022-09-15 16:14:58 -0400

    Fix clang-tidy stuff and try to fix formatting.
    Signed-off-by: Nathan Perry <nbperry@google.com>

--- test/user_defined_output/fake_plugin/fake_user_defined_output.cc
+++ test/user_defined_output/fake_plugin/fake_user_defined_output.cc
@@ -10,7 +10,7 @@ using ::nighthawk::FakeUserDefinedOutputConfig;
 FakeUserDefinedOutputPlugin::FakeUserDefinedOutputPlugin(
     FakeUserDefinedOutputConfig config,
     WorkerMetadata worker_metadata)
-    : config_(config), worker_metadata_(worker_metadata) {}
+    : config_(std::move(config)), worker_metadata_(std::move(worker_metadata)) {}
 
 absl::Status FakeUserDefinedOutputPlugin::handleResponseHeaders(const Envoy::Http::ResponseHeaderMapPtr&&) {
   headers_called_++;
