2be8e65a691b3d7dd802bb95d45c2b9849e6955c
Kevin Baichoo <kbaichoo@google.com>
2022-09-16 11:24:14 -0400

    Enhancements to support EDS churn test. Add single cluster EDS churn test. (#910)
    Signed-off-by: Kevin Baichoo <kbaichoo@google.com>

--- benchmarks/dynamic_config_envoy_proxy.py
+++ benchmarks/dynamic_config_envoy_proxy.py
@@ -2,29 +2,19 @@
 
 import pytest
 import logging
-import os
 from typing import Generator
 
 from test.integration import integration_test_fixtures, common, utility
 import envoy_proxy
 from dynamic_config import dynamic_config_server
-from nighthawk.api.configuration import cluster_config_manager_pb2
+from collections.abc import Callable
+from google.protobuf import message
 
 
 @pytest.fixture()
 def proxy_config() -> Generator[str, None, None]:
   """Yield the stock Envoy proxy configuration."""
   yield "nighthawk/benchmarks/configurations/envoy_proxy.yaml"
-
-
-@pytest.fixture()
-def dynamic_config_settings(
-) -> Generator[cluster_config_manager_pb2.DynamicClusterConfigManagerSettings, None, None]:
-  """Yield the stock Envoy proxy configuration."""
-  settings = cluster_config_manager_pb2.DynamicClusterConfigManagerSettings()
-  settings.refresh_interval.seconds = 5
-  settings.output_file = 'new_cds.pb'
-  yield settings
 
 
 class InjectDynamicHttpProxyIntegrationTestBase(envoy_proxy.InjectHttpProxyIntegrationTestBase):
@@ -37,9 +27,9 @@ class InjectDynamicHttpProxyIntegrationTestBase(envoy_proxy.InjectHttpProxyInteg
     See base class.
   """
 
-  def __init__(
-      self, request, server_config: str, proxy_config: str,
-      dynamic_config_settings: cluster_config_manager_pb2.DynamicClusterConfigManagerSettings):
+  def __init__(self, request, server_config: str, proxy_config: str,
+               dynamic_config_generator: Callable[[str, list[utility.SocketAddress]],
+                                                  message.Message]):
     """Initialize an InjectDynamicHttpProxyIntegrationTestBase.
 
     Arguments:
@@ -47,11 +37,11 @@ class InjectDynamicHttpProxyIntegrationTestBase(envoy_proxy.InjectHttpProxyInteg
         about the currently executing test case.
       server_config: Path to the server configuration.
       proxy_config: Path to the proxy configuration.
-      dynamic_config_settings: Settings for the dynamic configuration component.
+      dynamic_config_generator: Function for generating settings for the dynamic configuration component.
     """
     super(InjectDynamicHttpProxyIntegrationTestBase, self).__init__(request, server_config,
                                                                     proxy_config)
-    self._dynamic_config_settings = dynamic_config_settings
+    self._dynamic_config_generator = dynamic_config_generator
 
   def setUp(self):
     """Set up the injected Envoy proxy as well as the test server.
@@ -60,43 +50,14 @@ class InjectDynamicHttpProxyIntegrationTestBase(envoy_proxy.InjectHttpProxyInteg
     """
     super(InjectDynamicHttpProxyIntegrationTestBase, self).setUp()
 
-    output_file = os.path.join(self.test_server.tmpdir, self._dynamic_config_settings.output_file)
-    self._dynamic_config_settings.output_file = output_file
-    logging.info(f"Injecting dynamic configuration. Output file: {output_file}")
-
-    # TODO(kbaichoo): Don't hardcode the clusters based on path configuration.
-    if '15_listeners' in self._nighthawk_test_config_path:
-      clusters = [
-          'cluster_one',
-          'cluster_two',
-          'cluster_three',
-          'cluster_four',
-          'cluster_five',
-      ]
-    else:
-      clusters = ['service_envoyproxy_io']
-
-    self._assignEndpointsToClusters(clusters)
+    available_endpoints = utility.parseUrisToSocketAddress(self.getAllTestServerRootUris())
+    test_dir = self.test_server.tmpdir
+    dynamic_config_settings = self._dynamic_config_generator(test_dir, available_endpoints)
 
     self._dynamic_config_controller = dynamic_config_server.DynamicConfigController(
-        self._dynamic_config_settings)
-
+        dynamic_config_settings)
     assert (self._dynamic_config_controller.start())
     logging.info("dynamic configuration running")
-
-  def _assignEndpointsToClusters(self, clusters):
-    """Process all backend endpoints, round robin assigning to the given clusters."""
-    available_endpoints = utility.parseUrisToSocketAddress(self.getAllTestServerRootUris())
-
-    for cluster in clusters:
-      new_cluster = self._dynamic_config_settings.clusters.add()
-      new_cluster.name = cluster
-    cluster_idx = 0
-    for endpoint in available_endpoints:
-      new_endpoint = self._dynamic_config_settings.clusters[cluster_idx].endpoints.add()
-      new_endpoint.ip = endpoint.ip
-      new_endpoint.port = endpoint.port
-      cluster_idx = (cluster_idx + 1) % len(clusters)
 
   def tearDown(self, caplog):
     """Tear down the proxy and test server. Assert that both exit succesfully."""
@@ -106,22 +67,22 @@ class InjectDynamicHttpProxyIntegrationTestBase(envoy_proxy.InjectHttpProxyInteg
 
 @pytest.fixture(params=integration_test_fixtures.determineIpVersionsFromEnvironment())
 def inject_dynamic_envoy_http_proxy_fixture(request, server_config, proxy_config,
-                                            dynamic_config_settings, caplog):
+                                            dynamic_config_generator, caplog):
   """Injects a dynamically configured Envoy proxy in front of the test server.
 
   Arguments:
     request: The pytest `request` test fixture used to determine information about the currently executing test case.
-    server_config: path to the server configuration template.
-    proxy_config: path to the proxy configuration template.
-    dynamic_config_binary_path: path to the dynamic configuration binary.
-    dynamic_config_settings: settings used for the dynamic configuration.
+    server_config: Path to the server configuration template.
+    proxy_config: Path to the proxy configuration template.
+    dynamic_config_binary_path: Path to the dynamic configuration binary.
+    dynamic_config_generator: Function used to generate the settings for the configuration generator.
     caplog: The pytest `caplog` test fixture used to examine logged messages.
 
   Yields: a successfully set up InjectDynamicHttpProxyIntegrationTestBase
   instance.
   """
   fixture = InjectDynamicHttpProxyIntegrationTestBase(request, server_config, proxy_config,
-                                                      dynamic_config_settings)
+                                                      dynamic_config_generator)
   fixture.setUp()
   yield fixture
   fixture.tearDown(caplog)
--- benchmarks/dynamic_test/test_cds_churn_with_traffic.py
+++ benchmarks/dynamic_test/test_cds_churn_with_traffic.py
@@ -5,12 +5,38 @@ Test with Cluster churn with active traffic.
 """
 
 import pytest
+import os
 
-from dynamic_config_envoy_proxy import (dynamic_config_settings,
-                                        inject_dynamic_envoy_http_proxy_fixture, proxy_config)
-from benchmarks import utilities
+from dynamic_config_envoy_proxy import (inject_dynamic_envoy_http_proxy_fixture, proxy_config)
+from benchmarks import utilities as benchmarks_utilities
 from typing import Generator
 from rules_python.python.runfiles import runfiles
+from nighthawk.api.configuration import cluster_config_manager_pb2
+from test.integration import utility
+
+
+def _base_cds_config(
+    temp_dir: str) -> cluster_config_manager_pb2.DynamicClusterConfigManagerSettings:
+  settings = cluster_config_manager_pb2.DynamicClusterConfigManagerSettings()
+  settings.refresh_interval.seconds = 5
+  settings.output_file = os.path.join(temp_dir, 'new_cds.pb')
+  return settings
+
+
+# TODO(kbaichoo): migrate to common test utility
+def _assignEndpointsToClusters(
+    config: cluster_config_manager_pb2.DynamicClusterConfigManagerSettings, clusters: list[str],
+    available_endpoints: list[utility.SocketAddress]):
+  """Process all backend endpoints, round robin assigning to the given clusters."""
+  for cluster in clusters:
+    new_cluster = config.clusters.add()
+    new_cluster.name = cluster
+  cluster_idx = 0
+  for endpoint in available_endpoints:
+    new_endpoint = config.clusters[cluster_idx].endpoints.add()
+    new_endpoint.ip = endpoint.ip
+    new_endpoint.port = endpoint.port
+    cluster_idx = (cluster_idx + 1) % len(clusters)
 
 
 def _run_benchmark(fixture,
@@ -44,7 +70,7 @@ def _run_benchmark(fixture,
   parsed_json, _ = fixture.runNighthawkClient(args, expect_failure=True)
 
   # output test results
-  utilities.output_benchmark_results(parsed_json, fixture)
+  benchmarks_utilities.output_benchmark_results(parsed_json, fixture)
 
 
 def _readRunfile(path: str) -> str:
@@ -53,11 +79,19 @@ def _readRunfile(path: str) -> str:
     return f.read()
 
 
-# Test via injected Envoy
+def _config_generation_single_cluster(temp_dir: str, endpoints: list[utility.SocketAddress]):
+  """Configure CDS churn for a single cluster."""
+  config = _base_cds_config(temp_dir)
+  clusters = ['service_envoyproxy_io']
+  _assignEndpointsToClusters(config, clusters, endpoints)
+  return config
+
+
 @pytest.mark.parametrize('proxy_config',
                          ["nighthawk/benchmarks/configurations/dynamic_resources.yaml"])
 @pytest.mark.parametrize('server_config',
                          ["nighthawk/test/integration/configurations/nighthawk_http_origin.yaml"])
+@pytest.mark.parametrize('dynamic_config_generator', [_config_generation_single_cluster])
 def test_dynamic_http_single_cluster_traffic(inject_dynamic_envoy_http_proxy_fixture,
                                              proxy_config):  # noqa
   _run_benchmark(inject_dynamic_envoy_http_proxy_fixture)
@@ -69,11 +103,26 @@ def request_source_config() -> Generator[str, None, None]:
   yield "nighthawk/benchmarks/configurations/request_source_five_clusters.json"
 
 
+def _config_generation_five_cluster(temp_dir: str, endpoints: list[utility.SocketAddress]):
+  """Configure CDS churn for five clusters."""
+  config = _base_cds_config(temp_dir)
+  clusters = [
+      'cluster_one',
+      'cluster_two',
+      'cluster_three',
+      'cluster_four',
+      'cluster_five',
+  ]
+  _assignEndpointsToClusters(config, clusters, endpoints)
+  return config
+
+
 @pytest.mark.parametrize('proxy_config',
                          ["nighthawk/benchmarks/configurations/dynamic_resources.yaml"])
 @pytest.mark.parametrize(
     'server_config',
     ["nighthawk/test/integration/configurations/nighthawk_15_listeners_http_origin.yaml"])
+@pytest.mark.parametrize('dynamic_config_generator', [_config_generation_five_cluster])
 def test_dynamic_http_multiple_cluster_traffic(inject_dynamic_envoy_http_proxy_fixture,
                                                request_source_config, proxy_config):
   """Test that the nighthawkClient can run with request-source-plugin option."""
