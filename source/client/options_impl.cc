a9bc8f1864f07db22363d63817f7aedca7930b11
Kevin Baichoo <kbaichoo@google.com>
2022-11-05 17:29:34 +0000

    Update binary used in readme.
    Signed-off-by: Kevin Baichoo <kbaichoo@google.com>

--- source/client/options_impl.cc
+++ source/client/options_impl.cc
@@ -233,7 +233,8 @@ OptionsImpl::OptionsImpl(int argc, const char* const* argv) {
   TCLAP::MultiArg<std::string> failure_predicates(
       "", "failure-predicate",
       "Failure predicate. Allows specifying a counter name plus threshold value for "
-      "failing execution. Defaults to not tolerating error status codes and connection errors.",
+      "failing execution. Defaults to not tolerating error status codes and connection errors. "
+      "Example: benchmark.http_5xx:4294967295.",
       false, "string, uint64_t", cmd);
 
   std::vector<std::string> h1_connection_reuse_strategies = {"mru", "lru"};
