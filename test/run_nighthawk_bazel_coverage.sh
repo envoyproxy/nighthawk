fd0405eee65a1794698ea08531f346731f630bb4
Jakub Sobon <mumak@google.com>
2022-11-18 17:38:49 +0000

    Reverse argument order for zip.
    Signed-off-by: Jakub Sobon <mumak@google.com>

--- test/run_nighthawk_bazel_coverage.sh
+++ test/run_nighthawk_bazel_coverage.sh
@@ -43,7 +43,7 @@ cp bazel-out/_coverage/_coverage_report.dat "${COVERAGE_DATA}"
 COVERAGE_VALUE=$(genhtml --prefix ${PWD} --output "${COVERAGE_DIR}" "${COVERAGE_DATA}" | grep lines... | cut -d ' ' -f 4)
 COVERAGE_VALUE=${COVERAGE_VALUE%?}
 echo "Zipping coverage report to ${SRCDIR}/coverage_html.zip".
-zip -r "${COVERAGE_DIR}" "${SRCDIR}/coverage_html.zip"
+zip -r "${SRCDIR}/coverage_html.zip" "${COVERAGE_DIR}"
 
 [[ -z "${ENVOY_COVERAGE_DIR}" ]] || rsync -av "${COVERAGE_DIR}"/ "${ENVOY_COVERAGE_DIR}"
 
