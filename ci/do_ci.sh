f93ed2d9003291ea9c163c72ac4dcb66b4fe9992
Jakub Sobon <mumak@google.com>
2023-03-22 14:55:03 +0000

    Remove experimental bazel options.
    Signed-off-by: Jakub Sobon <mumak@google.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -284,8 +284,9 @@ fi
 export BAZEL_EXTRA_TEST_OPTIONS="--test_env=ENVOY_IP_TEST_VERSIONS=v4only ${BAZEL_EXTRA_TEST_OPTIONS}"
 export BAZEL_BUILD_OPTIONS=" \
 --verbose_failures ${BAZEL_OPTIONS} --action_env=HOME --action_env=PYTHONUSERBASE \
---experimental_local_memory_estimate \
---experimental_generate_json_trace_profile ${BAZEL_BUILD_EXTRA_OPTIONS}"
+${BAZEL_BUILD_EXTRA_OPTIONS}
+#--experimental_local_memory_estimate \
+#--experimental_generate_json_trace_profile ${BAZEL_BUILD_EXTRA_OPTIONS}"
 
 echo "Running with ${NUM_CPUS} cpus and BAZEL_BUILD_OPTIONS: ${BAZEL_BUILD_OPTIONS}"
 
