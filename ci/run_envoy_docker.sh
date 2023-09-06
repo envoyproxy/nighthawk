d245c832b9d744307a63e27e11ad28325f824044
eric846 <56563761+eric846@users.noreply.github.com>
2023-09-06 05:17:49 +0000

    revert to AZP_SHA1 variable
    Signed-off-by: eric846 <56563761+eric846@users.noreply.github.com>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -118,7 +118,7 @@ docker run --rm \
        "${ENVOY_DOCKER_OPTIONS[@]}" \
        "${VOLUMES[@]}" \
        -e AZP_BRANCH \
-       -e AZP_COMMIT_SHA \
+       -e AZP_SHA1 `# unique` \
        -e HTTP_PROXY \
        -e HTTPS_PROXY \
        -e NO_PROXY \
