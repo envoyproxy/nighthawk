6ff70c718259b3b5e7e0a8a9bf39101c6ccc48bc
Jakub Sobon <mumak@google.com>
2023-10-25 18:28:55 +0000

    Update spacing in docstrings.
    Signed-off-by: Jakub Sobon <mumak@google.com>

--- tools/format_python_tools.py
+++ tools/format_python_tools.py
@@ -37,10 +37,10 @@ def collectFiles(directory):
 def validateFormat(directory, fix=False):
   """Check the format of python files in the tools directory.
 
-    Args:
-      fix: a flag to indicate if fixes should be applied.
-    directory: A string, path to a directory where to look for files that should
-      be checked for correct formatting.
+  Args:
+    fix: a flag to indicate if fixes should be applied.
+    directory: A string, path to a directory where to look for files that
+      should be checked for correct formatting.
   """
   fixes_required = False
   failed_update_files = set()
