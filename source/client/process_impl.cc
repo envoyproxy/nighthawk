ea2a0cae5149f1238e53873f98c5a02cc6385b1a
qqustc <qqin@google.com>
2024-04-17 13:20:51 -0400

    Update Envoy to d417618 2024/04/15 (#1133)
    - sync files
    - add `# unique` in .bazelrc to avoid CI failure https://dev.azure.com/cncf/4684fb3d-0389-4e0b-8251-221942316e06/_apis/build/builds/168119/logs/57
    - add `cluster_manager_->initialize(bootstrap_);` to fix a CI crash which is introduced by https://github.com/envoyproxy/envoy/pull/33221 
    
    Signed-off-by: Qin Qin <qqin@google.com>

--- source/client/process_impl.cc
+++ source/client/process_impl.cc
@@ -902,6 +902,12 @@ bool ProcessImpl::runInternal(OutputCollector& collector, const UriPtr& tracing_
     }
     ENVOY_LOG(debug, "Computed configuration: {}", absl::StrCat(bootstrap_));
     cluster_manager_ = cluster_manager_factory_->clusterManagerFromProto(bootstrap_);
+    absl::Status status = cluster_manager_->initialize(bootstrap_);
+    if (!status.ok()) {
+      ENVOY_LOG(error, "cluster_manager initialize failed. Received bad status: {}",
+                status.message());
+      return false;
+    }
     maybeCreateTracingDriver(bootstrap_.tracing());
     cluster_manager_->setInitializedCb(
         [this]() -> void { init_manager_.initialize(init_watcher_); });
