ca2a4a45cc24cb321a286b8041717138c590a886
asingh-g <abhisinghx@google.com>
2025-06-25 19:59:44 +0000

    Update Port finding function to be cleaner
    Signed-off-by: asingh-g <abhisinghx@google.com>

--- source/client/options_impl.cc
+++ source/client/options_impl.cc
@@ -35,40 +35,64 @@ using ::nighthawk::client::Protocol;
 // allocated.
 uint16_t OptionsImpl::GetAvailablePort(bool udp) {
   int family = address_family_ == nighthawk::client::AddressFamily::V4 ? AF_INET : AF_INET6;
-  int sock = socket(family, udp ? SOCK_DGRAM : SOCK_STREAM, 0);
-    if(sock < 0) {
-      throw NighthawkException(absl::StrCat("could not create socket: ", strerror(errno)) );
+  int sock = socket(family, udp ? SOCK_DGRAM : SOCK_STREAM, udp ? 0 : IPPROTO_TCP);
+  if(sock < 0) {
+    throw NighthawkException(absl::StrCat("could not create socket: ", strerror(errno)) );
+    return 0;
+  }
+
+  // Reuseaddr lets us start up a server immediately after it exits
+  int one = 1;
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one)) < 0) {
+    throw NighthawkException(absl::StrCat("setsockopt: ", strerror(errno)));
+    close(sock);
+    return false;
+  }
+  sockaddr_storage storage;
+  size_t size;
+  if(family ==  AF_INET){
+    sockaddr_in* addr = reinterpret_cast<sockaddr_in*>(&storage);
+    size = sizeof(sockaddr_in);
+    memset(addr, 0, size);
+    addr->sin_family = AF_INET;
+    addr->sin_addr.s_addr = INADDR_ANY;
+    addr->sin_port = 0;
+    }
+  else {
+    sockaddr_in6* addr = reinterpret_cast<sockaddr_in6*>(&storage);
+    size = sizeof(sockaddr_in6);
+    memset(&addr, 0, size);
+    addr->sin6_family = AF_INET6;
+    addr->sin6_addr = in6addr_any;
+    addr->sin6_port = 0;  
+  }
+
+  if (bind(sock, reinterpret_cast<struct sockaddr *>(&storage), size) < 0) {
+      if(errno == EADDRINUSE) {
+          throw NighthawkException(absl::StrCat("Port allocated already in use"));
+      } else {     
+          throw NighthawkException(absl::StrCat("Could not bind to process: ", strerror(errno)) );
+      }
       return 0;
-    }
-    struct sockaddr_in serv_addr;
-    bzero(reinterpret_cast<char *>(&serv_addr), sizeof(serv_addr));
-    serv_addr.sin_family = family;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = 0;
-    if (bind(sock, reinterpret_cast<struct sockaddr *>(&serv_addr), sizeof(serv_addr)) < 0) {
-        if(errno == EADDRINUSE) {
-            throw NighthawkException(absl::StrCat("Port allocated already in use"));
-        } else {     
-            throw NighthawkException(absl::StrCat("Could not bind to process: ", strerror(errno)) );
-        }
-        return 0;
-    }
+  }
 
-    socklen_t len = sizeof(serv_addr);
-    if (getsockname(sock, reinterpret_cast<struct sockaddr *>(&serv_addr), &len) == -1) {
-        throw NighthawkException(absl::StrCat("Could not get sock name: ", strerror(errno)) );
-        return 0;
-    }
+  socklen_t len = size;
+  if (getsockname(sock, reinterpret_cast<struct sockaddr *>(&storage), &len) == -1) {
+      throw NighthawkException(absl::StrCat("Could not get sock name: ", strerror(errno)) );
+      return 0;
+  }
 
-    uint16_t port = ntohs(serv_addr.sin_port);
+  uint16_t port = ntohs(family ==  AF_INET ? reinterpret_cast<struct sockaddr_in *>(&storage)->sin_port
+  : reinterpret_cast<struct sockaddr_in6 *>(&storage)->sin6_port);
 
-    // close the socket, freeing the port to be used later.
-    if (close (sock) < 0 ) {
-        throw NighthawkException(absl::StrCat("Could not close socket: ", strerror(errno)) );
-        return 0;
-    }
+  // close the socket, freeing the port to be used later.
+  if (close (sock) < 0 ) {
+      throw NighthawkException(absl::StrCat("Could not close socket: ", strerror(errno)) );
+      return 0;
+  }
 
-    return port;
+  return port;
+
 }
 
 
