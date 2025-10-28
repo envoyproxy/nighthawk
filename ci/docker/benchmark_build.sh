7c0c35334e729e58404dc2e9d8e3bb067644f22b
tomjzzhang <4367421+tomjzzhang@users.noreply.github.com>
2025-10-28 18:06:08 +0000

    avoid copying of venv files to docker image
    Signed-off-by: tomjzzhang <4367421+tomjzzhang@users.noreply.github.com>

--- ci/docker/benchmark_build.sh
+++ ci/docker/benchmark_build.sh
@@ -21,7 +21,8 @@ TMP_DIR="$(mktemp -d ${WORKSPACE}/tmp-docker-build-context-XXXXXXXX)"
 
 echo "Preparing docker build context in ${TMP_DIR}"
 cp -r "${WORKSPACE}/ci/docker/" "${TMP_DIR}/"
-cp -rL "${BAZEL_BIN}/benchmarks" "${TMP_DIR}"
+# Exclude any venv files since they aren't necessary and docker can't handle symlinks.
+rsync -a --exclude='*.venv/' "${BAZEL_BIN}/benchmarks" "${TMP_DIR}"
 
 cd "${TMP_DIR}"
 echo "running docker build ... "
