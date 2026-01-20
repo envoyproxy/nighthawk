bb79877b29a1f71ec8a822d4151a833af89c335b
eric846 <56563761+eric846@users.noreply.github.com>
2026-01-20 15:43:25 +0000

    Updating Envoy version to e45201c (2026-01-17T17:46:45Z)
    See https://github.com/envoyproxy/envoy/commit/e45201c87d8481ecd72299139102d9ebf049cf75.
    
    Signed-off-by: eric846 <56563761+eric846@users.noreply.github.com>

--- tools/check_format.sh
+++ tools/check_format.sh
@@ -6,8 +6,8 @@ FULL_CHECK="$PWD"
 
 TO_CHECK="${2:-$FULL_CHECK}"
 # TODO(https://github.com/envoyproxy/nighthawk/issues/165): fully excluding everything
-# from the build fixer isn't ideal.
-bazel run @envoy//tools/code_format:check_format -- \
+# from the build fixer isn\'t ideal.
+bazel run --spawn_strategy=local --remote_executor= @envoy//tools/code_format:check_format -- \
   --config_path=${PWD}/tools/code_format/config.yaml \
   --skip_envoy_build_rule_check  --namespace_check Nighthawk \
   --build_fixer_check_excluded_paths=$TO_CHECK \
