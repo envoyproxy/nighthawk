31517e1a4d8b2112f6531920b74028df9d678d35
grayson-lafleur-google <193935438+Grayson-LaFleur-Google@users.noreply.github.com>
2026-03-02 14:47:18 +0000

    add untracked files
    Signed-off-by: grayson-lafleur-google <193935438+Grayson-LaFleur-Google@users.noreply.github.com>

---
+++ source/request_source/llm_request_source_plugin_impl.cc
@@ -0,0 +1,131 @@
+#include "source/request_source/llm_request_source_plugin_impl.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "absl/log/check.h"
+#include "absl/random/random.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_format.h"
+
+#include "source/request_source/llm_request_source_plugin.pb.h"
+
+#include "external/envoy/source/common/protobuf/protobuf.h"
+#include "external/envoy/source/common/protobuf/utility.h"
+#include "external/envoy/src/api/envoy/config/core/v3/base.pb.h"
+#include "external/envoy/src/api/envoy/config/core/v3/extension.pb.h"
+#include "external/envoy/src/envoy/api/api.h"
+#include "external/envoy/src/envoy/http/header_map.h"
+#include "external/envoy/src/envoy/registry/registry.h"
+#include "external/envoy/src/source/common/http/header_map_impl.h"
+#include "external/envoy/src/source/common/protobuf/utility.h"
+
+#include "api/client/options.pb.h"
+#include "api/request_source/request_source_plugin.pb.h"
+#include "source/common/request.h"
+#include "source/common/request_source.h"
+#include "nighthawk/request_source/request_source_plugin_config_factory.h"
+#include "source/common/request_impl.h"
+
+namespace nighthawk {
+namespace {
+
+absl::Status ValidateConfig(const LlmRequestSourcePluginConfig& config) {
+  if (config.model_name().empty()) {
+    return absl::InvalidArgumentError("Model name is required.");
+  }
+
+  return absl::OkStatus();
+}
+
+std::string GenerateRandomPrompt(int num_tokens) {
+  static const char charset[] =
+      "0123456789"
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      "abcdefghijklmnopqrstuvwxyz";
+
+  // The final string to be built.
+  std::string result_string;
+
+  // Random number generator for selecting characters from the charset.
+  absl::BitGen bitgen;
+
+  for (int i = 0; i < num_tokens; ++i) {
+    // Append a random character from the charset.
+    result_string += charset[absl::Uniform(bitgen, 0, 62)];
+
+    // Add a space between tokens. This is a naive way to calculate the number
+    // of tokens in the string as generally spaces delineate tokens.
+    if (i < num_tokens - 1) {
+      result_string += ' ';
+    }
+  }
+
+  return result_string;
+}
+
+}  // namespace
+
+Nighthawk::RequestGenerator LlmRequestSourcePlugin::get() {
+  return [this]() -> std::unique_ptr<Nighthawk::Request> {
+    Envoy::Http::RequestHeaderMapPtr headers =
+        Envoy::Http::RequestHeaderMapImpl::create();
+    Envoy::Http::HeaderMapImpl::copyFrom(*headers, *header_);
+
+    // TODO(b/436267941): Add support for multiple messages.
+    std::string body = absl::StrFormat(R"json(
+      {
+        "model": "%s",
+        "max_tokens": %d,
+        "messages": [
+          {
+            "role": "user",
+            "content": "%s"
+          }
+        ]
+      }
+    )json",
+                                       model_name_, resp_max_tokens_,
+                                       GenerateRandomPrompt(req_token_count_));
+
+    headers->setMethod(envoy::config::core::v3::RequestMethod_Name(
+        envoy::config::core::v3::RequestMethod::POST));
+    headers->setContentType("application/json");
+    headers->setContentLength(body.size());
+
+    auto path_key = Envoy::Http::LowerCaseString(":path");
+    headers->setCopy(path_key, "/v1/completions");
+
+    return std::make_unique<Nighthawk::RequestImpl>(std::move(headers), body);
+  };
+}
+
+Nighthawk::RequestSourcePtr LlmRequestSourcePluginFactory::createRequestSourcePlugin(
+    const Envoy::Protobuf::Message& message, Envoy::Api::Api&,
+    Envoy::Http::RequestHeaderMapPtr header) {
+  const auto* any = Envoy::Protobuf::DynamicCastToGenerated<const Envoy::Protobuf::Any>(&message);
+  LlmRequestSourcePluginConfig llm_config;
+  THROW_IF_NOT_OK(Envoy::MessageUtil::unpackTo(any, llm_config));
+  THROW_IF_NOT_OK(ValidateConfig(llm_config));
+
+  for (const nighthawk::client::RequestOptions& request_option :
+       llm_config.options_list().options()) {
+    for (const envoy::config::core::v3::HeaderValueOption& option_header :
+         request_option.request_headers()) {
+      auto lower_case_key =
+          Envoy::Http::LowerCaseString(option_header.header().key());
+      header->setCopy(lower_case_key, option_header.header().value());
+    }
+  }
+
+  return std::make_unique<LlmRequestSourcePlugin>(
+      std::string(llm_config.model_name()), llm_config.req_token_count(),
+      llm_config.resp_max_tokens(), std::move(header));
+};
+
+REGISTER_FACTORY(LlmRequestSourcePluginFactory,
+                 Nighthawk::RequestSourcePluginConfigFactory);
+
+}  // namespace nighthawk
---
+++ source/request_source/llm_request_source_plugin_impl.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "source/request_source/llm_request_source_plugin.pb.h"
+
+#include "absl/log/log.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+
+#include "external/envoy/src/api/envoy/config/core/v3/extension.pb.h"
+#include "external/envoy/src/envoy/api/api.h"
+#include "external/envoy/src/envoy/http/header_map.h"
+#include "external/envoy/src/source/common/common/logger.h"
+#include "external/envoy/src/source/common/protobuf/protobuf.h"
+#include "external/envoy/source/common/common/logger.h"
+
+#include "api/client/options.pb.h"
+#include "nighthawk/common/request_source.h"
+#include "nighthawk/request_source/request_source_plugin_config_factory.h"
+
+namespace nighthawk {
+
+constexpr inline absl::string_view kLlmRequestSourcePluginName =
+	"nighthawk.request_source.llm";
+
+// A Nighthawk RequestSource that generates requests for LLM inference.
+//
+// The request source generates requests with the following characteristics:
+//   - The request body is a JSON object with the following fields:
+//     - model: The name of the model to use for inference.
+//     - max_tokens: The maximum number of tokens to return in the response.
+//     - messages: A list with a single JSON object containing the following
+//       fields:
+//       - role: "user"
+//       - content: A string containing `req_token_count` randomly generated
+//         tokens.
+//   - The request headers are copied from the provided header map with the
+//     following modifications:
+//     - Method: POST
+//     - Content-Type: application/json
+//     - Content-Length: The length of the request body.
+//     - :path: /v1/completions
+class LlmRequestSourcePlugin
+    : public Nighthawk::RequestSource,
+      public Envoy::Logger::Loggable<Envoy::Logger::Id::http> {
+ public:
+  explicit LlmRequestSourcePlugin(std::string model_name, int req_token_count,
+                            int resp_max_tokens,
+                            Envoy::Http::RequestHeaderMapPtr header)
+      : model_name_(model_name),
+        req_token_count_(req_token_count),
+        resp_max_tokens_(resp_max_tokens),
+        header_(std::move(header)) {};
+
+  Nighthawk::RequestGenerator get() override;
+  void initOnThread() override {};
+  void destroyOnThread() override {};
+
+ private:
+  // Model to use for the request.
+  std::string model_name_;
+  // Number of tokens to generate in the request.
+  int req_token_count_;
+  // Maximum number of tokens from the model to return in the response.
+  int resp_max_tokens_;
+  // The options_list will be used to apply headers to the request.
+  std::unique_ptr<const nighthawk::client::RequestOptionsList> options_list_;
+  Envoy::Http::RequestHeaderMapPtr header_;
+};
+
+class LlmRequestSourcePluginFactory
+    : public virtual Nighthawk::RequestSourcePluginConfigFactory {
+ public:
+  std::string name() const override {
+    return std::string(kLlmRequestSourcePluginName);
+  }
+
+  Envoy::ProtobufTypes::MessagePtr createEmptyConfigProto() override {
+    return std::make_unique<LlmRequestSourcePluginConfig>();
+  }
+
+  Nighthawk::RequestSourcePtr createRequestSourcePlugin(
+      const Envoy::Protobuf::Message&, Envoy::Api::Api&,
+      Envoy::Http::RequestHeaderMapPtr header) override;
+};
+
+}  // namespace nighthawk
