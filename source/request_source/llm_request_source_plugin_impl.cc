52e164e13e60eaaf809f44ffa542fdd3b4436cf8
grayson-lafleur-google <193935438+Grayson-LaFleur-Google@users.noreply.github.com>
2026-03-06 20:55:32 +0000

    format
    Signed-off-by: grayson-lafleur-google <193935438+Grayson-LaFleur-Google@users.noreply.github.com>

--- source/request_source/llm_request_source_plugin_impl.cc
+++ source/request_source/llm_request_source_plugin_impl.cc
@@ -51,7 +51,7 @@ std::string GenerateRandomPrompt(int num_tokens) {
   for (int i = 0; i < num_tokens; ++i) {
     // Append a random character from the charset.
     absl::StrAppend(&result_string,
-		    std::string(1, kCharset[absl::Uniform<size_t>(bitgen, 0, kCharset.length())]));
+                    std::string(1, kCharset[absl::Uniform<size_t>(bitgen, 0, kCharset.length())]));
 
     // Add a space between tokens. This is a naive way to calculate the number
     // of tokens in the string as generally spaces delineate tokens.
