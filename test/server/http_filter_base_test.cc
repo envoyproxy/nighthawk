873869ec57c2d8ae98ea7ef6ef9b11c3f322a97e
Jason Ye <jiajunye@google.com>
2026-05-30 03:20:11 +0000

    fix formats
    Signed-off-by: Jason Ye <jiajunye@google.com>

--- test/server/http_filter_base_test.cc
+++ test/server/http_filter_base_test.cc
@@ -31,7 +31,8 @@ public:
   ResponseOrigin getHappyFlowResponseOrigin() {
     // Modulo the test-server, extensions are expected to need an upstream to synthesize a reply
     // when the effective configuration is valid.
-    return config_.find("name: test-server") != std::string::npos ? ResponseOrigin::EXTENSION : ResponseOrigin::UPSTREAM;
+    return config_.find("name: test-server") != std::string::npos ? ResponseOrigin::EXTENSION
+	                                                          : ResponseOrigin::UPSTREAM;
   }
 
 protected:
