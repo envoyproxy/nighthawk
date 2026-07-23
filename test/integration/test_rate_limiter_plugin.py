c2e235bd358cfa94041c0c59c42d2207a73114cf
Josh Gu <joshgu@google.com>
2026-07-23 14:14:46 +0000

    fix tsan test failure
    Signed-off-by: Josh Gu <joshgu@google.com>

--- test/integration/test_rate_limiter_plugin.py
+++ test/integration/test_rate_limiter_plugin.py
@@ -10,23 +10,23 @@ from test.integration import asserts
 def test_linear_ramping_rate_limiter_plugin(http_test_server_fixture):
   """Test LinearRampingRateLimiter plugin.
 
-  Ramp time: 5.5 seconds
-  Duration: 12 seconds
-  RPS: 100
-  Expected total requests: 925 (margin of 5)
+  Ramp time: 5.4 seconds
+  Duration: 10 seconds
+  RPS: 10
+  Expected total requests: 73 (margin of 3)
   """
   rate_limiter_config = (
       "{name:\"nighthawk.linear-ramping-rate-limiter-plugin\","
       "typed_config:{\"@type\":\"type.googleapis.com/nighthawk.rate_limiter.LinearRampingRateLimiterConfig\","
-      "ramp_time:{seconds:5,nanos:500000000}}}")  # 5.5 seconds
+      "ramp_time:{seconds:5,nanos:400000000}}}")  # 5.4 seconds
 
   parsed_json, _ = http_test_server_fixture.runNighthawkClient([
-      http_test_server_fixture.getTestServerRootUri(), "--duration", "12", "--concurrency", "1",
-      "--rps", "100", "--rate-limiter-plugin-config", rate_limiter_config
+      http_test_server_fixture.getTestServerRootUri(), "--duration", "10", "--concurrency", "1",
+      "--rps", "10", "--rate-limiter-plugin-config", rate_limiter_config
   ])
   counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
 
-  expected_total = 925
-  margin = 5
+  expected_total = 73
+  margin = 3
   asserts.assertCounterBetweenInclusive(counters, "benchmark.http_2xx", expected_total - margin,
                                         expected_total + margin)
