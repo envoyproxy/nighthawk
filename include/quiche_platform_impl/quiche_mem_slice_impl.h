560ee693cdd452ba522fcb0c208370a8e40cffc5
fei-deng <101672975+fei-deng@users.noreply.github.com>
2026-09-15 11:32:28 +0000

    Add forwarding headers and quiche_platform_impl_headers deps to source/ targets for RBE remote sandboxing
    Signed-off-by: fei-deng <101672975+fei-deng@users.noreply.github.com>

---
+++ include/quiche_platform_impl/quiche_mem_slice_impl.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Forwarding header for Envoy QUIC platform mem slice implementation.
+#include "external/envoy/source/common/quic/platform/quiche_platform_impl/quiche_mem_slice_impl.h" // NOLINT(namespace-nighthawk)
