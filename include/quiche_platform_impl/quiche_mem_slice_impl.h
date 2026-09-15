9965bc79a84e5698f746c1aa7c09fd267624c944
fei-deng <101672975+fei-deng@users.noreply.github.com>
2026-09-15 11:46:41 +0000

    Update forwarding headers to use external/envoy+ canonical path for Bzlmod RBE sandbox resolution
    Signed-off-by: fei-deng <101672975+fei-deng@users.noreply.github.com>

--- include/quiche_platform_impl/quiche_mem_slice_impl.h
+++ include/quiche_platform_impl/quiche_mem_slice_impl.h
@@ -1,4 +1,4 @@
 #pragma once
 
 // Forwarding header for Envoy QUIC platform mem slice implementation.
-#include "external/envoy/source/common/quic/platform/quiche_platform_impl/quiche_mem_slice_impl.h" // NOLINT(namespace-nighthawk)
+#include "external/envoy+/source/common/quic/platform/quiche_platform_impl/quiche_mem_slice_impl.h" // NOLINT(namespace-nighthawk)
