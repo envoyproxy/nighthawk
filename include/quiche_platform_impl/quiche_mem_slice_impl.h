530c160a910037b1fb30b34dd9e66ebcd014414b
fei-deng <101672975+fei-deng@users.noreply.github.com>
2026-09-15 07:46:39 +0000

    Update forwarding headers to use external/envoy path for RBE include resolution
    Signed-off-by: fei-deng <101672975+fei-deng@users.noreply.github.com>

--- include/quiche_platform_impl/quiche_mem_slice_impl.h
+++ include/quiche_platform_impl/quiche_mem_slice_impl.h
@@ -2,4 +2,4 @@
 
 // NOLINT(namespace-nighthawk)
 // Forwarding header to reuse Envoy's QUICHE platform implementation directly.
-#include "source/common/quic/platform/quiche_platform_impl/quiche_mem_slice_impl.h"
+#include "external/envoy/source/common/quic/platform/quiche_platform_impl/quiche_mem_slice_impl.h"
