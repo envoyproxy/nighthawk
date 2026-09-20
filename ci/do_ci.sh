a6128b39472bc6766fb61520188cd35c1d4ef8b2
fei-deng <101672975+fei-deng@users.noreply.github.com>
2026-09-20 23:40:58 +0000

    ci: add -c fastbuild --copt=-g0 --strip=always to test_gcc in do_ci.sh
    Signed-off-by: fei-deng <101672975+fei-deng@users.noreply.github.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -347,7 +347,8 @@ case "$1" in
     ;;
     test_gcc)
         setup_gcc_toolchain
-        bazel build $BAZEL_BUILD_OPTIONS //:nighthawk
+        BAZEL_BUILD_OPTIONS="$BAZEL_BUILD_OPTIONS --copt=-g0 --strip=always"
+        bazel build -c fastbuild $BAZEL_BUILD_OPTIONS //:nighthawk
         exit 0
     ;;
     clang_tidy)
