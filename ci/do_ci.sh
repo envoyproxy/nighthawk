784e362664c2c9a742b403f9c49eb892607c6388
fei-deng <101672975+fei-deng@users.noreply.github.com>
2026-09-21 18:27:03 +0000

    ci: restore //:nighthawk target (all 5 binaries) for test_gcc in do_ci.sh
    Signed-off-by: fei-deng <101672975+fei-deng@users.noreply.github.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -348,7 +348,7 @@ case "$1" in
     test_gcc)
         setup_gcc_toolchain
         BAZEL_BUILD_OPTIONS="$BAZEL_BUILD_OPTIONS --copt=-g0 --strip=always"
-        bazel build -c fastbuild $BAZEL_BUILD_OPTIONS //:nighthawk_client
+        bazel build -c fastbuild $BAZEL_BUILD_OPTIONS //:nighthawk
         exit 0
     ;;
     clang_tidy)
