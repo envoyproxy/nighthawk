658ffd2b58e1e24393a2847658dfc2868137d2e8
Jason Ye <jiajunye@google.com>
2026-09-23 19:54:56 +0000

    split gcc into two parts to reduce resource consumption
    Signed-off-by: Jason Ye <jiajunye@google.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -348,7 +348,10 @@ case "$1" in
     test_gcc)
         setup_gcc_toolchain
         BAZEL_BUILD_OPTIONS="$BAZEL_BUILD_OPTIONS --copt=-g0 --strip=always"
-        bazel build -c fastbuild $BAZEL_BUILD_OPTIONS //:nighthawk_client //:nighthawk_service //:nighthawk_output_transform //:nighthawk_adaptive_load_client
+        # Stage 1: Build client and output transform first (compiles and caches common dependencies)
+        bazel build -c fastbuild $BAZEL_BUILD_OPTIONS //:nighthawk_client //:nighthawk_output_transform
+        # Stage 2: Build service and adaptive load client
+        bazel build -c fastbuild $BAZEL_BUILD_OPTIONS //:nighthawk_service //:nighthawk_adaptive_load_client
         exit 0
     ;;
     clang_tidy)
